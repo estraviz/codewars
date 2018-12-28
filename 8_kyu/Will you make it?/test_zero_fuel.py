bda31570698984ec8329e38e693eac4cbeb2a870
estraviz <javier.estraviz@gmail.com>
2018-12-28 23:59:39 +0100

    Updating tests

--- 8_kyu/Will you make it?/test_zero_fuel.py
+++ 8_kyu/Will you make it?/test_zero_fuel.py
@@ -2,7 +2,7 @@ from zero_fuel import zero_fuel
 
 
 def test_zero_fuel():
-    assert zero_fuel(50, 25, 2) == True
-    assert zero_fuel(60, 30, 3) == True
-    assert zero_fuel(70, 25, 1) == False
-    assert zero_fuel(100, 25, 3) == False
+    assert zero_fuel(50, 25, 2) is True
+    assert zero_fuel(60, 30, 3) is True
+    assert zero_fuel(70, 25, 1) is False
+    assert zero_fuel(100, 25, 3) is False
