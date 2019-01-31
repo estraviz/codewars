b3c2424df375369cc33f4ebb7a81e16396dfbf24
estraviz <javier.estraviz@gmail.com>
2019-01-31 01:13:47 +0100

    Refactor of given code

--- 8_kyu/Squash the bugs/find_longest.py
+++ 8_kyu/Squash the bugs/find_longest.py
@@ -3,11 +3,4 @@
 
 
 def find_longest(string):
-    spl = string.split()
-    longest = 0
-    i = 0
-    while (i < len(spl)):
-        if len(spl[i]) > longest:
-            longest = len(spl[i])
-        i += 1
-    return longest
+    return max(list(map(lambda x: len(x), string.split(" "))))
