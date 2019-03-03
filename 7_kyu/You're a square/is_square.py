f530e7dcbb077990eae4028800352fd8fa9c6497
estraviz <javier.estraviz@gmail.com>
2019-03-03 20:32:36 +0100

    Refactor of kata: You're a square

--- 7_kyu/You're a square/is_square.py
+++ 7_kyu/You're a square/is_square.py
@@ -4,5 +4,4 @@ from math import sqrt
 
 
 def is_square(n):
-    return n == sum(2*k - 1 for k in range(1, int(sqrt(n)) + 1)) \
-        if n >= 0 else False
+    return n > 0 and sqrt(n).is_integer()
