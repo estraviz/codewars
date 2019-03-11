61b9417bf560db1c602c52af4b2ab23a41f2356d
estraviz <javier.estraviz@gmail.com>
2019-03-11 20:07:38 +0100

    Simplifying kata code: Sum of odd numbers

--- 7_kyu/Sum of odd numbers/row_sum_add_numbers.py
+++ 7_kyu/Sum of odd numbers/row_sum_add_numbers.py
@@ -3,6 +3,5 @@
 
 
 def row_sum_odd_numbers(n):
-    # the complicated form in the first commit
-    return n > 0 and \
-           sum([num for num in range(n**2 - (n - 1), n**2 + (n + 1), 2)])
+    # the simple form in the 2nd commit, just elementary maths knowledge
+    return n**3
