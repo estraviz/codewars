f6bfbe517624960df2583ba764a915155d89aea7
estraviz <javier.estraviz@gmail.com>
2019-03-17 14:57:30 +0100

    Improved solution to kata: Last

--- 7_kyu/Last/last.py
+++ 7_kyu/Last/last.py
@@ -3,10 +3,7 @@
 
 
 def last(*args):
-    if len(args) == 1:
-        try:
-            last = args[0][-1]
-            return last
-        except TypeError:
-            return args[0]
-    return args[-1]
+    try:
+        return args[-1][-1]
+    except:
+        return args[-1]
