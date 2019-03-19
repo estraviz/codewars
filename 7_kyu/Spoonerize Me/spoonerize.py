12dbb42a6aa8282f26c7f66eb085d398d8047841
estraviz <javier.estraviz@gmail.com>
2019-03-19 09:55:45 +0100

    Refactor of 'Spoonerize Me' kata with f-strings (available from Python 3.6)

--- 7_kyu/Spoonerize Me/spoonerize.py
+++ 7_kyu/Spoonerize Me/spoonerize.py
@@ -3,5 +3,5 @@
 
 
 def spoonerize(words):
-    word1, word2 = words.split()
-    return word2[0] + word1[1:] + " " + word1[0] + word2[1:]
+    w0, w1 = words.split()
+    return f'{w1[0] + w0[1:]} {w0[0] + w1[1:]}'
