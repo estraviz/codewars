8ab5af726a364e4f32c10dcaae26f9faead62d48
estraviz <javier.estraviz@gmail.com>
2019-03-22 00:53:24 +0100

    Possible refactor of kata: Job Matching #2

--- 7_kyu/Job Matching #2/match.py
+++ 7_kyu/Job Matching #2/match.py
@@ -3,13 +3,25 @@
 
 
 def match(job, candidates):
-    filtered_candidates = []
+    matched_candidates = []
     for candidate in candidates:
-        if not candidate['desires_equity'] or \
-               candidate['desires_equity'] and job['equity_max'] > 0:
-            for job_location in job['locations']:
-                if job_location in candidate['current_location'] or \
-                   job_location in candidate['desired_locations']:
-                    filtered_candidates.append(candidate)
-                    break
-    return filtered_candidates
+        if matching_by_equity(job['equity_max'], candidate['desires_equity']) \
+           and matching_by_location(job['locations'],
+                                    [candidate['current_location']] +
+                                    candidate['desired_locations']):
+            matched_candidates.append(candidate)
+    return matched_candidates
+
+
+def matching_by_equity(equity_max, desires_equity):
+    return not desires_equity or equity_max
+
+
+def matching_by_location(locations, current_or_desired_locations):
+    for location in locations:
+        if location in current_or_desired_locations:
+            match = True
+            break
+    else:
+        match = False
+    return match
