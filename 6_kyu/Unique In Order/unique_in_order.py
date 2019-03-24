0d2e8b5b0172b4b31e504b59dbff2e5b6c6be6e1
estraviz <javier.estraviz@gmail.com>
2019-03-25 00:58:19 +0100

    Refactor of kata: 'Unique In Order' by using itertools function 'groupby'

--- 6_kyu/Unique In Order/unique_in_order.py
+++ 6_kyu/Unique In Order/unique_in_order.py
@@ -1,13 +1,7 @@
 """Unique In Order
 """
+from itertools import groupby
 
 
 def unique_in_order(iterable):
-    output = []
-    if len(iterable) == 0:
-        return output
-    output = [iterable[0]]
-    for i in range(1, len(iterable)):
-        if iterable[i] != output[-1]:
-            output.append(iterable[i])
-    return output
+    return [key for key, group in groupby(iterable)]
