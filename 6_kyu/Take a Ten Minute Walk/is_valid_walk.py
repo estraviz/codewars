01ee4b5c030cd5f37fe3644667416c3b684df19f
estraviz <javier.estraviz@gmail.com>
2019-03-27 00:09:05 +0100

    Refactor: simple version of kata 'Take a Ten Minute Walk'

--- 6_kyu/Take a Ten Minute Walk/is_valid_walk.py
+++ 6_kyu/Take a Ten Minute Walk/is_valid_walk.py
@@ -1,24 +1,8 @@
 """Take a Ten Minute Walk
 """
-from collections import Counter
-
-NORTH = 'n'
-EAST = 'e'
-SOUTH = 's'
-WEST = 'w'
 
 
 def is_valid_walk(walk):
-    return True if \
-        is_len_of_walk_x(walk, 10) and \
-        is_num_steps_equal(walk, NORTH, SOUTH) and \
-        is_num_steps_equal(walk, EAST, WEST) else False
-
-
-def is_len_of_walk_x(walk, x):
-    return len(walk) == x
-
-
-def is_num_steps_equal(walk, dir1, dir2):
-    steps = Counter(walk)
-    return steps[dir1] == steps[dir2]
+    return len(walk) == 10 and \
+           walk.count('n') == walk.count('s') and \
+           walk.count('e') == walk.count('w')
