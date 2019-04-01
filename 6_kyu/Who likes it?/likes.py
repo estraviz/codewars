171fe230a84d733dc94b1481b4b19c30ff0b1348
estraviz <javier.estraviz@gmail.com>
2019-04-02 00:27:33 +0200

    Refactor of kata: 'Who likes it?' with multiple unpacking

--- 6_kyu/Who likes it?/likes.py
+++ 6_kyu/Who likes it?/likes.py
@@ -3,15 +3,9 @@
 
 
 def likes(names):
-    if len(names) == 0:
-        return "no one likes this"
-    elif len(names) == 1:
-        return "".join([names[0], " likes this"])
-    elif len(names) == 2:
-        return "".join([names[0], " and ", names[1], " like this"])
-    elif len(names) == 3:
-        return "".join([names[0], ", ", names[1], " and ", names[2],
-                       " like this"])
-
-    return "".join([names[0], ", ", names[1], " and ", str(len(names) - 2),
-                   " others like this"])
+    cases = {0: 'no one likes this',
+             1: '{} likes this',
+             2: '{} and {} like this',
+             3: '{}, {} and {} like this',
+             4: '{}, {} and {others} others like this'}
+    return cases[min(4, len(names))].format(*names, others=len(names)-2)
--- 6_kyu/Who likes it?/test_likes.py
+++ 6_kyu/Who likes it?/test_likes.py
@@ -1,11 +1,22 @@
 from likes import likes
 
 
-def test_likes():
+def test_no_name():
     assert likes([]) == 'no one likes this'
+
+
+def test_one_name():
     assert likes(['Peter']) == 'Peter likes this'
+
+
+def test_two_names():
     assert likes(['Jacob', 'Alex']) == 'Jacob and Alex like this'
+
+
+def test_three_names():
     assert likes(['Max', 'John', 'Mark']) == 'Max, John and Mark like this'
 
+
+def test_four_or_more_names():
     message = 'Alex, Jacob and 2 others like this'
     assert likes(['Alex', 'Jacob', 'Mark', 'Max']) == message
