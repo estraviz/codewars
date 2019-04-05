2e448c50e6e2cf2bffec07369e95cc12bf1d3aa6
estraviz <javier.estraviz@gmail.com>
2019-04-05 18:39:33 +0200

    Refactor of kata: 'Create Phone Number' using asterisk to unpack the entry array

--- 6_kyu/Create Phone Number/create_phone_number.py
+++ 6_kyu/Create Phone Number/create_phone_number.py
@@ -3,5 +3,4 @@
 
 
 def create_phone_number(n):
-    num = "".join(str(num) for num in n)
-    return "({}) {}-{}".format(num[:3], num[3:6], num[6:])
+    return '({}{}{}) {}{}{}-{}{}{}{}'.format(*n)
