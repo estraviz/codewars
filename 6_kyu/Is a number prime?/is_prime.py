9622ee4dea188dd3d3cbb74af53872685daec3d1
estraviz <javier.estraviz@gmail.com>
2019-04-09 00:34:30 +0200

    Refactor of kata: Is a number prime?

--- 6_kyu/Is a number prime?/is_prime.py
+++ 6_kyu/Is a number prime?/is_prime.py
@@ -1,13 +1,12 @@
 """Is a number prime?
 """
+from math import sqrt
 
 
 def is_prime(num):
-    if num <= 1:
+    if num < 2:
         return False
-    num_var = 2
-    while num_var < num:
-        if num % num_var == 0:
+    for i in range(2, int(sqrt(num) + 1)):
+        if num % i == 0:
             return False
-        num_var += 1
     return True
