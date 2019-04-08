7dffdd6e1e568c54ccc2cb9b1f2f6949875145ae
estraviz <javier.estraviz@gmail.com>
2019-04-09 00:36:47 +0200

    New refactor of kata (faster, skip even numbers checking): Is a number prime?

--- 6_kyu/Is a number prime?/is_prime.py
+++ 6_kyu/Is a number prime?/is_prime.py
@@ -4,9 +4,11 @@ from math import sqrt
 
 
 def is_prime(num):
-    if num < 2:
+    if num == 2:
+        return True
+    if num < 2 or num % 2 == 0:
         return False
-    for i in range(2, int(sqrt(num) + 1)):
+    for i in range(3, int(sqrt(num) + 1), 2):
         if num % i == 0:
             return False
     return True
