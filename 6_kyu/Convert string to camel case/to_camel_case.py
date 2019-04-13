d657ef583e1f14fdb341bc17ae91f2c5987d0ade
estraviz <javier.estraviz@gmail.com>
2019-04-14 00:01:06 +0200

    Refactor of kata: Convert string to camel case

--- 6_kyu/Convert string to camel case/to_camel_case.py
+++ 6_kyu/Convert string to camel case/to_camel_case.py
@@ -1,10 +1,7 @@
 """Convert string to camel case
 """
-import re
 
 
 def to_camel_case(text):
-    return "" if len(text) == 0 \
-      else "".join([word if i == 0 and word[0] == word[0].lower()
-                    else word.capitalize()
-                    for i, word in enumerate(re.split(r'-|_', text))])
+    text = text.replace('_', '-').split('-')
+    return text[0] + "".join(x.title() for x in text[1:])
