752c4c932c50743a9a7a1953abe22ad169913193
estraviz <javier.estraviz@gmail.com>
2019-04-20 00:49:51 +0200

    Refactor of kata: 'String Average' by using mean() function

--- 6_kyu/String average/average_string.py
+++ 6_kyu/String average/average_string.py
@@ -1,15 +1,14 @@
 """Average string
 """
+from statistics import mean
 
 
 def average_string(s):
     numbers = {'zero': 0, 'one': 1, 'two': 2, 'three': 3, 'four': 4, 'five': 5,
                'six': 6, 'seven': 7, 'eight': 8, 'nine': 9}
+    inv_numbers = {v: k for k, v in numbers.items()}
     try:
-        accum = 0
-        for num in s.split():
-            accum += numbers[num]
-        inv_numbers = {v: k for k, v in numbers.items()}
-        return inv_numbers[accum//len(s.split())]
-    except (KeyError, ZeroDivisionError):
+        accum = [numbers[num] for num in s.split()]
+        return inv_numbers[int(mean(accum))]
+    except (ValueError, KeyError):
         return 'n/a'
