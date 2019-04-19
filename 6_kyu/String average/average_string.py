ac15440858946cb38e9d571ed2400585b909a9f4
estraviz <javier.estraviz@gmail.com>
2019-04-20 00:36:39 +0200

    Refactor of kata: String Average

--- 6_kyu/String average/average_string.py
+++ 6_kyu/String average/average_string.py
@@ -3,14 +3,13 @@
 
 
 def average_string(s):
-    if not s:
-        return "n/a"
     numbers = {'zero': 0, 'one': 1, 'two': 2, 'three': 3, 'four': 4, 'five': 5,
                'six': 6, 'seven': 7, 'eight': 8, 'nine': 9}
-    accum = 0
-    for num in s.split():
-        if num not in numbers.keys():
-            return "n/a"
-        accum += numbers[num]
-    inv_numbers = {v: k for k, v in numbers.items()}
-    return inv_numbers[accum//len(s.split())]
+    try:
+        accum = 0
+        for num in s.split():
+            accum += numbers[num]
+        inv_numbers = {v: k for k, v in numbers.items()}
+        return inv_numbers[accum//len(s.split())]
+    except (KeyError, ZeroDivisionError):
+        return 'n/a'
