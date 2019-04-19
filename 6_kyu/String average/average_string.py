a7f032e359020dec14549b888f8b97fe54094315
estraviz <javier.estraviz@gmail.com>
2019-04-20 00:08:18 +0200

    Refactor of kata: String Average

--- 6_kyu/String average/average_string.py
+++ 6_kyu/String average/average_string.py
@@ -3,15 +3,14 @@
 
 
 def average_string(s):
-    if len(s.split()) == 0:
+    if not s:
         return "n/a"
     numbers = {'zero': 0, 'one': 1, 'two': 2, 'three': 3, 'four': 4, 'five': 5,
                'six': 6, 'seven': 7, 'eight': 8, 'nine': 9}
-    sum = 0
+    accum = 0
     for num in s.split():
-        if num not in numbers:
+        if num not in numbers.keys():
             return "n/a"
-        sum += numbers[num]
-    for k, v in numbers.items():
-        if v == sum//len(s.split()):
-            return k
+        accum += numbers[num]
+    inv_numbers = {v: k for k, v in numbers.items()}
+    return inv_numbers[accum//len(s.split())]
--- 6_kyu/String average/readme.md
+++ 6_kyu/String average/readme.md
@@ -4,6 +4,8 @@
 
 You are given a string of numbers between 0-9. Find the average of these numbers and return it as a floored whole number (ie: no decimal places) written out as a string. Eg:
 
+```python
 "zero nine five two" -> "four"
+```
 
 If the string is empty or includes a number greater than 9, return "n/a"
