e4c0fcaea54b4c609d84c4ab3714001c2960e51a
estraviz <javier.estraviz@gmail.com>
2019-05-05 00:28:29 +0200

    Refactor of kata 'Simple Pig Latin' by using string.isalpha() method

--- 5_kyu/Simple Pig Latin/pig_it.py
+++ 5_kyu/Simple Pig Latin/pig_it.py
@@ -1,10 +1,7 @@
 """Simple Pig Latin
 """
-from string import ascii_letters
 
 
-def pig_it(text):
-    return " ".join(["".join([word[1:],
-                              word[0],
-                              'ay' if word[0] in ascii_letters else ''])
-                     for word in text.split()])
+def pig_it(txt):
+    return " ".join(word if not word.isalpha() else
+                    "".join([word[1:], word[0], 'ay']) for word in txt.split())
