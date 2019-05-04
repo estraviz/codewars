185113fda4012d3656cea27c06cdbebda9079ead
estraviz <javier.estraviz@gmail.com>
2019-05-05 00:02:51 +0200

    Refactor of cata 'Simple Pig Latin' with list comprehensions and considering string.punctuation

--- 5_kyu/Simple Pig Latin/pig_it.py
+++ 5_kyu/Simple Pig Latin/pig_it.py
@@ -1,16 +1,10 @@
 """Simple Pig Latin
 """
+from string import punctuation
 
 
 def pig_it(text):
-    import string
-    output = []
-    for word in text.split():
-        if word in string.punctuation:
-            output.append(word)
-        else:
-            if word[-1] in string.punctuation:
-                output.append("".join([word[1:-1], word[0], "ay", word[-1]]))
-            else:
-                output.append("".join([word[1:], word[0], "ay"]))
-    return " ".join(output)
+    return " ".join(["".join([word[1:], word[0], 'ay'])
+                    if word.strip(punctuation)
+                    else "".join([word[1:], word[0]])
+                    for word in text.split()])
