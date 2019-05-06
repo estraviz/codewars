53b5bcf33c0ef624645fa28f042cabf3e4024193
estraviz <javier.estraviz@gmail.com>
2019-05-07 01:21:27 +0200

    A tiny refactor of kata: 'Abbreviate a Two Word Name'

--- 8_kyu/Abbreviate a Two Word Name/abbrev_name.py
+++ 8_kyu/Abbreviate a Two Word Name/abbrev_name.py
@@ -3,5 +3,5 @@
 
 
 def abbrev_name(name):
-    name_list = name.upper().split()
-    return f'{name_list[0][0]}.{name_list[1][0]}'
+    first, last = name.upper().split()
+    return f'{first[0]}.{last[0]}'
