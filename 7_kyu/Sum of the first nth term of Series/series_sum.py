3f1e21f26c6bc5ade18bf3831d3241868ab705e4
estraviz <javier.estraviz@gmail.com>
2019-05-09 23:08:12 +0200

    Small refactor for kata: 'Sum of the first nth term of Series'

--- 7_kyu/Sum of the first nth term of Series/series_sum.py
+++ 7_kyu/Sum of the first nth term of Series/series_sum.py
@@ -3,5 +3,4 @@
 
 
 def series_sum(n):
-    return '0.00' if n == 0 \
-                  else f'{round(sum([1/(1+3*i) for i in range(n)]), 2):.2f}'
+    return f'{round(sum([1/(1+3*i) for i in range(n)]), 2):.2f}'
