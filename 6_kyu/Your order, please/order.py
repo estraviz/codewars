644c292869d5bdb66ed11d50455fdefd82e0ea20
estraviz <javier.estraviz@gmail.com>
2019-05-12 01:47:08 +0200

    Refactor of kata: 'Your order, please' to a concise solution that uses sorted() as key, which returns digit in each word first and orders accordingly

--- 6_kyu/Your order, please/order.py
+++ 6_kyu/Your order, please/order.py
@@ -1,10 +1,6 @@
 """Your order, please
 """
-from collections import OrderedDict
 
 
 def order(sentence):
-    positions = [ch for ch in sentence if ch.isdigit()]
-    tuples = dict((idx, wrd) for idx, wrd in zip(positions, sentence.split()))
-    ordered = OrderedDict(sorted(tuples.items(), key=lambda x: x[0]))
-    return " ".join(ordered.values())
+    return " ".join(sorted(sentence.split(), key=sorted))
