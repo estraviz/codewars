152317756e0f245bef09dbe194bded7431c0cb8e
estraviz <javier.estraviz@gmail.com>
2019-05-31 23:23:52 +0200

    Adding kata: 'Are they the same?'

---
+++ 6_kyu/Are they the same?/comp.py
@@ -0,0 +1,8 @@
+"""Are they the "same"?
+"""
+
+
+def comp(array1, array2):
+    if array1 is None or array2 is None:
+        return False
+    return sorted([num**2 for num in array1]) == sorted(array2)
