c4d390e4a6e3f2edff9e5663f39a2bfda8ec8222
estraviz <javier.estraviz@gmail.com>
2019-06-14 00:50:21 +0200

    Refactor for kata: 'Exclamation marks series #6: Remove n exclamation marks in the sentence from left to right'

--- 8_kyu/Exclamation marks series #6: Remove n exclamation marks in the sentence from left to right/remove.py
+++ 8_kyu/Exclamation marks series #6: Remove n exclamation marks in the sentence from left to right/remove.py
@@ -5,17 +5,4 @@ Exclamation marks series #6:
 
 
 def remove(s, n):
-    count = 0
-    new_s = ""
-    for i, chr in enumerate(s):
-        if count == n:
-            if i + 1 == len(s):
-                return new_s
-            else:
-                return new_s + s[i:]
-        else:
-            if chr == '!':
-                count += 1
-            else:
-                new_s += chr
-    return new_s
+    return s.replace('!', '', n)
