c1e249a2cd62bbbc77209acfd96e9c7e754c9de1
estraviz <javier.estraviz@gmail.com>
2019-06-17 00:57:04 +0200

    Refactor of kata: 'A Strange Trip to the Market' by using any() function

--- 8_kyu/A Strange Trip to the Market/is_lock_ness_monster.py
+++ 8_kyu/A Strange Trip to the Market/is_lock_ness_monster.py
@@ -4,7 +4,4 @@ A Strange Trip to the Market
 
 
 def is_lock_ness_monster(string):
-    for expr in ["tree fiddy", "3.50", "three fifty"]:
-        if string.__contains__(expr):
-            return True
-    return False
+    return any(txt in string for txt in ["tree fiddy", "3.50", "three fifty"])
