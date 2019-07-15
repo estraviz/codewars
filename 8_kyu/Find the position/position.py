29cdf173c2bf81779e6e9c7ff0ff6c7f947e773f
estraviz <javier.estraviz@gmail.com>
2019-07-15 12:25:29 +0200

    Refactor of kata: 'Find the position!'

--- 8_kyu/Find the position/position.py
+++ 8_kyu/Find the position/position.py
@@ -1,9 +1,7 @@
 """
-Find the position?
+Find the position!
 """
-
-from string import ascii_lowercase
 
 
 def position(alphabet):
-    return f'Position of alphabet: {ascii_lowercase.index(alphabet.lower())+1}'
+    return f"Position of alphabet: {ord(alphabet.lower()) - ord('a') + 1}"
