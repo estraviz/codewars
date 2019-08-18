dc97efd604e2bbf72f7da4213e2997bbfd8a58dc
estraviz <javier.estraviz@gmail.com>
2019-08-18 19:49:05 +0200

    After small refactor of kata: 'Coding 3min: Father and Son'

--- 7_kyu/Coding 3min: Father and Son/sc.py
+++ 7_kyu/Coding 3min: Father and Son/sc.py
@@ -4,6 +4,4 @@ Coding 3min: Father and Son
 
 
 def sc(s):
-    return ''.join([
-        letter for letter in s if letter.upper() in s and letter.lower() in s
-    ])
+    return ''.join(letter for letter in s if letter.swapcase() in s)
