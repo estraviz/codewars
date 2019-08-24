b67c49806f9f054a84a89ce0b5faf7087bf2f0e3
estraviz <javier.estraviz@gmail.com>
2019-08-24 20:42:04 +0200

    Refactor of kata: 'Difference of Volumes of Cuboids', this time using reduce function from functools and lambda functions

--- 8_kyu/Difference of Volumes of Cuboids/find_difference.py
+++ 8_kyu/Difference of Volumes of Cuboids/find_difference.py
@@ -2,8 +2,12 @@
 Difference of Volumes of Cuboids
 """
 
-from numpy import prod
+from functools import reduce
 
 
 def find_difference(a, b):
-    return abs(prod(a) - prod(b))
+    return abs(get_volume(a) - get_volume(b))
+
+
+def get_volume(lst):
+    return reduce(lambda x, y: x * y, lst)
