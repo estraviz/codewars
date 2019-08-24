ae82d2fa2002adcbad6aeaf8dc6413baa7aa2872
estraviz <javier.estraviz@gmail.com>
2019-08-24 20:44:02 +0200

    Adding more test cases to kata: 'Difference of Volumes of Cuboids'

--- 8_kyu/Difference of Volumes of Cuboids/test_find_difference.py
+++ 8_kyu/Difference of Volumes of Cuboids/test_find_difference.py
@@ -4,3 +4,6 @@ from find_difference import find_difference
 def test_find_difference():
     assert find_difference([3, 2, 5], [1, 4, 4]) == 14
     assert find_difference([9, 7, 2], [5, 2, 2]) == 106
+    assert find_difference([11, 2, 5], [1, 10, 8]) == 30
+    assert find_difference([4, 4, 7], [3, 9, 3]) == 31
+    assert find_difference([15, 20, 25], [10, 30, 25]) == 0
