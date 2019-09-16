885b12a4d26e3a52b20a4738396f13b011b3cbb2
estraviz <javier.estraviz@gmail.com>
2019-09-16 23:58:15 +0200

    After refactor of kata: 'Well of Ideas - Harder Version' by using chain from_iterable (itertools)

--- 7_kyu/Well of Ideas - Harder Version/well.py
+++ 7_kyu/Well of Ideas - Harder Version/well.py
@@ -2,14 +2,16 @@
 Well of Ideas - Harder Version
 """
 
+from itertools import chain
+
 
 def well(arr):
-    count = [
-        elem.lower() if type(elem) == str else '' for inner_arr in arr
-        for elem in inner_arr
+    num_good = [
+        elem.lower() if type(elem) == str else ''
+        for elem in chain.from_iterable(arr)
     ].count('good')
     return {
         0: 'Fail!',
         1: 'Publish!',
         2: 'Publish!'
-    }.get(count, 'I smell a series!')
+    }.get(num_good, 'I smell a series!')
