20449e8d3053c07b15ceaf4070f720b9565600e1
estraviz <javier.estraviz@gmail.com>
2019-09-21 00:27:09 +0200

    Refactor of kata: 'Incrementer'

--- 7_kyu/Incrementer/incrementer.py
+++ 7_kyu/Incrementer/incrementer.py
@@ -4,12 +4,4 @@ Incrementer
 
 
 def incrementer(nums):
-    output = []
-    if nums:
-        for i, num in enumerate(nums):
-            aux = i + 1 + num
-            len_aux = len(str(aux))
-            if len_aux > 1:
-                aux = aux % ((len_aux - 1) * 10)
-            output.append(aux)
-    return output
+    return [(num + i + 1) % 10 for i, num in enumerate(nums)] if nums else []
