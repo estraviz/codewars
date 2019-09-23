1f3481366f67d8a5ee663cb74511b1944e6ba1b9
estraviz <javier.estraviz@gmail.com>
2019-09-24 00:58:34 +0200

    After refactor of kata: 'Nice Array'

--- 7_kyu/Nice Array/is_nice.py
+++ 7_kyu/Nice Array/is_nice.py
@@ -4,22 +4,9 @@ Nice Array
 
 
 def is_nice(arr):
-    if arr:
-        for i, elem in enumerate(arr):
-            new_arr = arr.copy()
-            new_arr.pop(i)
-            if is_item_in_list(elem - 1, new_arr) or is_item_in_list(
-                    elem + 1, new_arr):
-                continue
-            else:
-                return False
-        return True
-    return False
-
-
-def is_item_in_list(item, lst):
-    try:
-        pos = lst.index(item)
-        return True
-    except ValueError:
+    if not arr:
         return False
+    for elem in arr:
+        if elem - 1 not in arr and elem + 1 not in arr:
+            return False
+    return True
