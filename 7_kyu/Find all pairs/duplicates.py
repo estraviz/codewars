20bc39d9b3821373c2727d8da2c1b157c4c5ffd1
estraviz <javier.estraviz@gmail.com>
2019-09-25 01:28:13 +0200

    After refactor of kata: 'Find all pairs'

--- 7_kyu/Find all pairs/duplicates.py
+++ 7_kyu/Find all pairs/duplicates.py
@@ -4,10 +4,4 @@ Find all pairs
 
 
 def duplicates(arr):
-    dic = {}
-    for num in arr:
-        if num in dic:
-            dic[num] += 1
-        else:
-            dic[num] = 1
-    return sum(value // 2 for value in dic.values() if value > 1)
+    return sum(arr.count(num) // 2 for num in set(arr))
