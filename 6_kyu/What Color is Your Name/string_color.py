71870c36313bc278f3302cc65e0c20aa325a6c36
estraviz <javier.estraviz@gmail.com>
2019-09-29 00:29:09 +0200

    Improved version after refactor for kata: 'What Color is Your Name?', which uses f-strings with hex format

--- 6_kyu/What Color is Your Name/string_color.py
+++ 6_kyu/What Color is Your Name/string_color.py
@@ -6,19 +6,11 @@ from functools import reduce
 
 
 def string_color(name):
-    if not name or len(name) < 2:
-        return None
-
-    ascii_values = [ord(letter) for letter in name]
-
-    first = check_leading_zero(hex(sum(ascii_values) % 256)[2:])
-    second = check_leading_zero(
-        hex(reduce(lambda x, y: x * y, ascii_values) % 256)[2:])
-    third = check_leading_zero(
-        hex(abs(ascii_values[0] - sum(ascii_values[1:])) % 256)[2:])
-
-    return ''.join([first, second, third])
-
-
-def check_leading_zero(hex_num):
-    return str(hex_num).rjust(2, '0').upper()
+    if len(name) >= 2:
+        asciis = [ord(letter) for letter in name]
+        return ''.join(
+            map(lambda x: f'{x % 256:02X}', [
+                sum(asciis),
+                reduce(lambda x, y: x * y, asciis),
+                abs(asciis[0] - sum(asciis[1:]))
+            ]))
