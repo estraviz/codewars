093ab93bb13720362ed3cbe61a9b25b03cce0ad6
estraviz <javier.estraviz@gmail.com>
2019-09-29 12:41:40 +0200

    Small refactor

--- 6_kyu/Reverse every other word in the string/reverse_alternate.py
+++ 6_kyu/Reverse every other word in the string/reverse_alternate.py
@@ -5,6 +5,5 @@ Reverse every other word in the string
 
 def reverse_alternate(string):
     return ' '.join([
-        word[::-1] if i % 2 != 0 else word
-        for i, word in enumerate(string.split())
+        word[::-1] if i % 2 else word for i, word in enumerate(string.split())
     ])
