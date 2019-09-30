be0a5cc6b6a9c0b9fb671bc2b3dd4ba2d2e5810c
estraviz <javier.estraviz@gmail.com>
2019-09-30 23:25:34 +0200

    Adding more tests to kata: 'Numericals of a String'

--- 6_kyu/Numericals of a String/test_numericals.py
+++ 6_kyu/Numericals of a String/test_numericals.py
@@ -8,3 +8,9 @@ def test_numericals():
     assert numericals("hello hello") == "11121122342"
     assert numericals("Hello") == "11121"
     assert numericals("aaaaaaaaaaaa") == "123456789101112"
+    assert numericals("11111") == "12345"
+    assert numericals("hope you 123456789 expected numbers in the string"
+                      ) == "1111112121111111113212311414121151151262267232231"
+    assert numericals(
+        "In this string, I'll make sure the amounts of a character go over 9"
+    ) == "11111112221221132112411115312263237221234482193101343525441123124155131"
