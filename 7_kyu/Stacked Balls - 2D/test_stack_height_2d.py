3210652464cf5326f74b55b8a43f7c1cc4427376
estraviz <javier.estraviz@gmail.com>
2019-10-05 11:55:08 +0200

    Adding tests for kata: 'Stacked Balls - 2D'

--- 7_kyu/Stacked Balls - 2D/test_stack_height_2d.py
+++ 7_kyu/Stacked Balls - 2D/test_stack_height_2d.py
@@ -4,3 +4,10 @@ from stack_height_2d import stack_height_2d
 def test_stacked_height_2d():
     assert stack_height_2d(1) == 1.0
     assert stack_height_2d(2) == 1.866
+    assert stack_height_2d(68) == 59.024
+    assert stack_height_2d(51) == 44.301
+    assert stack_height_2d(26) == 22.651
+    assert stack_height_2d(67) == 58.158
+    assert stack_height_2d(46) == 39.971
+    assert stack_height_2d(33) == 28.713
+    assert stack_height_2d(11) == 9.66
