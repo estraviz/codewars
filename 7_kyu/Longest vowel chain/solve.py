573d4d6a1cc593f137dabc5da8b97bb72031d8fc
estraviz <javier.estraviz@gmail.com>
2019-10-07 23:47:05 +0200

    After refactor of kata: 'Longest vowel chain' by using regex

--- 7_kyu/Longest vowel chain/solve.py
+++ 7_kyu/Longest vowel chain/solve.py
@@ -2,14 +2,8 @@
 Longest vowel chain
 """
 
+import re
+
 
 def solve(s):
-    count = max_count = 0
-    for c in s:
-        if c in 'aeiou':
-            count += 1
-            if count > max_count:
-                max_count = count
-        else:
-            count = 0
-    return max_count
+    return max(len(x) for x in re.findall(r"[aeiou]+", s))
