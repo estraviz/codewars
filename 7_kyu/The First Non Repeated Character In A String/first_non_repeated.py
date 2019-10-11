da23afcbfcde758ac80a94f49eeb33f8d991be64
estraviz <javier.estraviz@gmail.com>
2019-10-11 22:53:46 +0200

    Adding improved version for kata: 'The First Non Repeated Character In A String'

--- 7_kyu/The First Non Repeated Character In A String/first_non_repeated.py
+++ 7_kyu/The First Non Repeated Character In A String/first_non_repeated.py
@@ -4,12 +4,6 @@ The First Non Repeated Character In A String
 
 
 def first_non_repeated(s):
-    times = {}
     for c in s:
-        if c in times:
-            times[c] += 1
-        else:
-            times[c] = 1
-    for c in s:
-        if times[c] == 1:
+        if s.count(c) == 1:
             return c
