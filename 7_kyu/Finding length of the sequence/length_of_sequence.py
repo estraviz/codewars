f2ab4c2d2506c195e9ef5f9360a9e25cea1b434d
estraviz <javier.estraviz@gmail.com>
2019-10-16 00:39:46 +0200

    Refactor for kata: 'Finding length of the sequence' by using generators

--- 7_kyu/Finding length of the sequence/length_of_sequence.py
+++ 7_kyu/Finding length of the sequence/length_of_sequence.py
@@ -6,9 +6,7 @@ Finding length of the sequence
 def length_of_sequence(arr, n):
     if arr.count(n) != 2:
         return 0
-    for i, c in enumerate(arr):
-        if n == c:
-            try:
-                return i - first + 1
-            except:
-                first = i
+    else:
+        gen = (i for i, c in enumerate(arr) if c == n)
+        first = next(gen)
+        return next(gen) - first + 1
