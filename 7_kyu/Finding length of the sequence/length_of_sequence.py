b8cd80ef4e2b26af137267b239d73c67555068b0
estraviz <javier.estraviz@gmail.com>
2019-10-15 23:58:21 +0200

    Adding kata: 'Finding length of the sequence'

---
+++ 7_kyu/Finding length of the sequence/length_of_sequence.py
@@ -0,0 +1,14 @@
+"""
+Finding length of the sequence
+"""
+
+
+def length_of_sequence(arr, n):
+    if arr.count(n) < 2:
+        return 0
+    for i, c in enumerate(arr):
+        if n == c:
+            try:
+                return i - first + 1
+            except:
+                first = i
