3b20d67af3bb55297ac54367d984fe9b51aa4a6b
estraviz <javier.estraviz@gmail.com>
2019-10-19 01:31:01 +0200

    Adding more tests for kata: 'Which section did you scroll to?'

--- 7_kyu/Which section did you scroll to?/test_get_section_id.py
+++ 7_kyu/Which section did you scroll to?/test_get_section_id.py
@@ -2,6 +2,8 @@ from get_section_id import get_section_id
 
 
 def test_get_section_id():
+    assert get_section_id(1, [300, 200, 400, 600, 100]) == 0
     assert get_section_id(299, [300, 200, 400, 600, 100]) == 0
     assert get_section_id(300, [300, 200, 400, 600, 100]) == 1
+    assert get_section_id(1599, [300, 200, 400, 600, 100]) == 4
     assert get_section_id(1600, [300, 200, 400, 600, 100]) == -1
