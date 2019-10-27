439f5a1d96aa34a44d766e3c5a2168a43f63371d
estraviz <javier.estraviz@gmail.com>
2019-10-28 00:15:06 +0100

    Small refactor for kata: 'Remove duplicate words'

--- 7_kyu/Remove duplicate words/remove_duplicate_words.py
+++ 7_kyu/Remove duplicate words/remove_duplicate_words.py
@@ -4,11 +4,8 @@ Remove duplicate words
 
 
 def remove_duplicate_words(s):
-    words = s.split()
     unique_words = []
-    for word in words:
-        if word in unique_words:
-            continue
-        else:
+    for word in s.split():
+        if word not in unique_words:
             unique_words.append(word)
     return ' '.join(unique_words)
