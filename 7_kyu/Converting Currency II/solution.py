d0c04b95aa884e5448ef5367038f7b7018da5a85
estraviz <javier.estraviz@gmail.com>
2019-10-30 23:43:28 +0100

    A second (and more complicated but still interesting) approach to solution with dictionaries and map function, for kata: 'Converting Currency II'

--- 7_kyu/Converting Currency II/solution.py
+++ 7_kyu/Converting Currency II/solution.py
@@ -4,10 +4,13 @@ Converting Currency II
 
 
 def solution(to_cur, values):
-    eur_to_usd = 1.1363636
-
-    if to_cur == 'USD':
-        return [f'${round(value * eur_to_usd, 2):,.2f}' for value in values]
-
-    if to_cur == 'EUR':
-        return [f'{round(value / eur_to_usd, 2):,.2f}€' for value in values]
+    currencies = {'USD': (0, '$', 1.1363636), 'EUR': (-1, '€', 1 / 1.1363636)}
+    try:
+        (pos, symbol, factor) = currencies.get(to_cur)
+        return list(
+            map(
+                lambda value:
+                f'{symbol if pos == 0 else ""}{round(value * factor, 2):,.2f}{symbol if pos == -1 else ""}',
+                values))
+    except KeyError:
+        return None
