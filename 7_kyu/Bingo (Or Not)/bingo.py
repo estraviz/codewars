b251065f0dd461c1391e2276d4fb8adc3862ee31
estraviz <javier.estraviz@gmail.com>
2019-10-31 23:47:44 +0100

    After refactor for kata: 'Bingo (Or Not)'

--- 7_kyu/Bingo (Or Not)/bingo.py
+++ 7_kyu/Bingo (Or Not)/bingo.py
@@ -4,8 +4,5 @@ Bingo (Or Not)
 
 
 def bingo(array):
-    chrs = list(map(lambda x: chr(x + 64), array))
-    for letter in 'BINGO':
-        if letter not in chrs:
-            return "LOSE"
-    return "WIN"
+    return "WIN" if set("BINGO").issubset(
+        set(map(lambda x: chr(x + 64), array))) else "LOSE"
