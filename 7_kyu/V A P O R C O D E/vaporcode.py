7ed396f2e56a5ddc42080318759c731c7402fa1e
estraviz <javier.estraviz@gmail.com>
2019-11-02 00:10:03 +0100

    After refactor for kata: 'V A P O R C O D E'

--- 7_kyu/V A P O R C O D E/vaporcode.py
+++ 7_kyu/V A P O R C O D E/vaporcode.py
@@ -2,17 +2,6 @@
 V A P O R C O D E
 """
 
-from string import ascii_letters
-
 
 def vaporcode(s):
-    str = ""
-    for chr in s:
-        if chr in ascii_letters:
-            str += chr.upper()
-        else:
-            if chr == ' ':
-                continue
-            else:
-                str += chr
-    return '  '.join(str)
+    return '  '.join(c.upper() for c in s if c != ' ')
