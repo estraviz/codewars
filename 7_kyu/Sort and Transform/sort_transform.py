857cee7356fb2563b02904fcbb6a2f31da0d3040
estraviz <javier.estraviz@gmail.com>
2019-11-11 00:14:11 +0100

    Refactor for kata: 'Sort and Transform'

--- 7_kyu/Sort and Transform/sort_transform.py
+++ 7_kyu/Sort and Transform/sort_transform.py
@@ -4,20 +4,16 @@ Sort and Transform
 
 
 def sort_transform(arr):
-    arr_aux = arr
-    new_arr = [chr(arr_aux[0]), chr(arr_aux[1]), chr(arr_aux[-2]), chr(arr_aux[-1])]
-    part_one = ''.join(new_arr)
+    e1 = ''.join(compose_chr_list(arr))
+    e2 = ''.join(compose_chr_list(sorted(arr)))
+    e3 = ''.join(compose_chr_list(sorted(arr, reverse=True)))
+    e4 = ''.join(compose_list(sorted([chr(x) for x in arr])))
+    return '-'.join([e1, e2, e3, e4])
 
-    arr_aux = sorted(arr)
-    new_arr = [chr(arr_aux[0]), chr(arr_aux[1]), chr(arr_aux[-2]), chr(arr_aux[-1])]
-    part_two = ''.join(new_arr)
 
-    arr_aux = sorted(arr, reverse=True)
-    new_arr = [chr(arr_aux[0]), chr(arr_aux[1]), chr(arr_aux[-2]), chr(arr_aux[-1])]
-    part_three = ''.join(new_arr)
+def compose_list(arr_aux):
+    return [arr_aux[0], arr_aux[1], arr_aux[-2], arr_aux[-1]]
 
-    arr_aux = sorted([chr(x) for x in arr])
-    new_arr = [arr_aux[0], arr_aux[1], arr_aux[-2], arr_aux[-1]]
-    part_four = ''.join(new_arr)
 
-    return '-'.join([part_one, part_two, part_three, part_four])
+def compose_chr_list(arr):
+    return [chr(arr[0]), chr(arr[1]), chr(arr[-2]), chr(arr[-1])]
