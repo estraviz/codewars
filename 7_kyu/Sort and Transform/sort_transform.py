25806bacb2ac276f2b496e29477a277d6a96c8d6
estraviz <javier.estraviz@gmail.com>
2019-11-11 00:38:54 +0100

    Refactor for kata: 'Sort and Transform'

--- 7_kyu/Sort and Transform/sort_transform.py
+++ 7_kyu/Sort and Transform/sort_transform.py
@@ -4,9 +4,9 @@ Sort and Transform
 
 
 def sort_transform(arr):
-    e1 = ''.join(get_word(arr))
-    e2 = ''.join(get_word(sorted(arr)))
-    e3 = ''.join(get_word(sorted(arr, reverse=True)))
+    e1 = get_word(arr)
+    e2 = get_word(sorted(arr))
+    e3 = get_word(sorted(arr, reverse=True))
     e4 = e2
     return '-'.join([e1, e2, e3, e4])
 
@@ -16,4 +16,4 @@ def compose_list(arr):
 
 
 def get_word(arr):
-    return list(map(chr, compose_list(arr)))
+    return ''.join(list(map(chr, compose_list(arr))))
