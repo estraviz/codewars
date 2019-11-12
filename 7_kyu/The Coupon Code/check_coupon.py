c9eeed230388c90ed1b5baad608f658fed528616
estraviz <javier.estraviz@gmail.com>
2019-11-13 00:53:26 +0100

    Refactor for kata: 'The Coupon Code'

--- 7_kyu/The Coupon Code/check_coupon.py
+++ 7_kyu/The Coupon Code/check_coupon.py
@@ -6,13 +6,6 @@ from dateutil.parser import parse
 
 
 def check_coupon(entered_code, correct_code, current_date, expiration_date):
-    return True if valid_date(current_date, expiration_date) and valid_coupon(
-        entered_code, correct_code) else False
-
-
-def valid_date(date1, date2):
-    return parse(date1) <= parse(date2)
-
-
-def valid_coupon(coupon1, coupon2):
-    return coupon1 == coupon2 and type(coupon1) == type(coupon2)
+    return True if parse(current_date) <= parse(
+        expiration_date) and entered_code == correct_code and type(
+            entered_code) == type(correct_code) else False
