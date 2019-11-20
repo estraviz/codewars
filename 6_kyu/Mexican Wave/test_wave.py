c7cd0b10d479ef7c22f454632771ac4d4a5d29b9
estraviz <javier.estraviz@gmail.com>
2019-11-20 01:20:12 +0100

    After refactor for kata: 'Mexican Wave'

--- 6_kyu/Mexican Wave/test_wave.py
+++ 6_kyu/Mexican Wave/test_wave.py
@@ -22,3 +22,15 @@ def test_wave():
 
     result = [" Gap ", " gAp ", " gaP "]
     assert wave(" gap ") == result
+
+    result = ["A       b    ", "a       B    "]
+    assert wave("a       b    ") == result
+
+    result = [
+        "This is a few words", "tHis is a few words", "thIs is a few words",
+        "thiS is a few words", "this Is a few words", "this iS a few words",
+        "this is A few words", "this is a Few words", "this is a fEw words",
+        "this is a feW words", "this is a few Words", "this is a few wOrds",
+        "this is a few woRds", "this is a few worDs", "this is a few wordS"
+    ]
+    assert wave("this is a few words") == result
--- 6_kyu/Mexican Wave/wave.py
+++ 6_kyu/Mexican Wave/wave.py
@@ -2,14 +2,9 @@
 Mexican Wave
 """
 
-from string import ascii_lowercase
-
 
 def wave(str):
-    lst = []
-    i = 0
-    for letter in str:
-        if letter in ascii_lowercase:
-            lst.append(''.join(str[:i] + str[i].capitalize() + str[i + 1:]))
-        i += 1
-    return lst
+    return [
+        str[:i] + str[i].upper() + str[i + 1:] for i in range(len(str))
+        if str[i].isalpha()
+    ]
