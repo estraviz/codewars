341d82bee37edf67630509d7e9b610ae55c0f840
estraviz <javier.estraviz@gmail.com>
2019-11-21 00:39:27 +0100

    Adding more tests to kata: 'Simple calculator'

--- 8_kyu/Simple calculator/test_calculator.py
+++ 8_kyu/Simple calculator/test_calculator.py
@@ -1,9 +1,19 @@
 from calculator import calculator
 
 
-def test_calculator():
+def test_simple():
     assert calculator(6, 2, '+') == 8
     assert calculator(4, 3, '-') == 1
     assert calculator(5, 5, '*') == 25
     assert calculator(5, 4, '/') == 1.25
+
+
+def test_error():
     assert calculator(6, 2, '&') == "unknown value"
+    assert calculator(6, "$", '+') == "unknown value"
+    assert calculator(6, 2, '&') == "unknown value"
+    assert calculator(4, 3, '\\') == "unknown value"
+    assert calculator("a", 3, "+") == "unknown value"
+    assert calculator(6, 2, '=') == "unknown value"
+    assert calculator(6, 2, '\t') == "unknown value"
+    assert calculator(":", ",", '+') == "unknown value"
