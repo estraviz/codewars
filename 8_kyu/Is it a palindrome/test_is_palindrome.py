41f8c2283595b2af8c7725cf45891ba0393abbf1
estraviz <javier.estraviz@gmail.com>
2019-12-05 23:52:05 +0100

    Adding more tests to kata: 'Is it a palindrome?'

--- 8_kyu/Is it a palindrome/test_is_palindrome.py
+++ 8_kyu/Is it a palindrome/test_is_palindrome.py
@@ -9,3 +9,18 @@ def test_sample_tests():
     assert is_palindrome('walter') is False
     assert is_palindrome('kodok') is True
     assert is_palindrome('Kasue') is False
+    assert is_palindrome('hello') is False
+    assert is_palindrome('Bob') is True
+    assert is_palindrome('Madam') is True
+    assert is_palindrome('AbBa') is True
+    assert is_palindrome('') is True
+    assert is_palindrome("LAGrALLyiclOaEowNvmU") is False
+    assert is_palindrome("cWalaIYFGucHEhbnEH") is False
+    assert is_palindrome("smlWLKQYCrRUcqOLYuGGuYLOqcURrCYQKLWlms") is True
+    assert is_palindrome("dRjLeHcvvcHeLjRd") is True
+    assert is_palindrome("wvvqHAfrFWkIYygRQHTR") is False
+    assert is_palindrome("zuKWoAyeQNvhurRlYlUUlYlRruhvNQeyAoWKuz") is True
+    assert is_palindrome("QtFpQMSYPMnnMPYSMQpFtQ") is True
+    assert is_palindrome("muNcdggdcNum") is True
+    assert is_palindrome("TUkKinLuE") is False
+    assert is_palindrome("MaKeRSDisini") is False
