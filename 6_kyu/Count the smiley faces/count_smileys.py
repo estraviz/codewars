dade6da6db74bdd2ac240237a048a0dfcaa9014b
estraviz <javier.estraviz@gmail.com>
2019-12-08 10:28:59 +0100

    Adding refactor for kata: 'Count the smiley faces!'

--- 6_kyu/Count the smiley faces/count_smileys.py
+++ 6_kyu/Count the smiley faces/count_smileys.py
@@ -4,26 +4,19 @@ Count the smiley faces!
 
 
 def count_smileys(arr):
-    if not arr:
-        return 0
-    eyes = {':', ';'}
-    noses = {'-', '~'}
-    mouths = {')', 'D'}
     count = 0
-    for smiley in arr:
-        if smiley == '':
-            continue
-        eye = smiley[0]
-        if eye not in eyes:
-            continue
-        if len(smiley) == 3:
-            nose = smiley[1]
-            if nose not in noses:
+    if arr:
+        eyes, noses, mouths = [{':', ';'}, {'-', '~'}, {')', 'D'}]
+        for smiley in arr:
+            if len(smiley) == 2:
+                eye, mouth = list(smiley)
+            elif len(smiley) == 3:
+                eye, nose, mouth = list(smiley)
+                if nose not in noses:
+                    continue
+            else:
                 continue
-            mouth = smiley[2]
-        else:
-            mouth = smiley[1]
-        if mouth not in mouths:
-            continue
-        count += 1
+            if eye not in eyes or mouth not in mouths:
+                continue
+            count += 1
     return count
