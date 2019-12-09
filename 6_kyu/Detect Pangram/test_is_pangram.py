95bcfd7fb4d9042adc9af54a0e2d1fdae48ef769
estraviz <javier.estraviz@gmail.com>
2019-12-09 23:08:47 +0100

    After refactor for kata: 'Detect Pangram'. More tests added too.

--- 6_kyu/Detect Pangram/test_is_pangram.py
+++ 6_kyu/Detect Pangram/test_is_pangram.py
@@ -4,3 +4,23 @@ from is_pangram import is_pangram
 def test_is_pangram():
     pangram = "The quick, brown fox jumps over the lazy dog!"
     assert is_pangram(pangram) is True
+
+    pangram = "Cwm fjord bank glyphs vext quiz"
+    assert is_pangram(pangram) is True
+
+    pangram = "Pack my box with five dozen liquor jugs."
+    assert is_pangram(pangram) is True
+
+    pangram = "How quickly daft jumping zebras vex."
+    assert is_pangram(pangram) is True
+
+    pangram = "ABCD45EFGH,IJK,LMNOPQR56STUVW3XYZ"
+    assert is_pangram(pangram) is True
+
+
+def test_is_not_a_pangram():
+    pangram = "This isn't a pangram!"
+    assert is_pangram(pangram) is False
+
+    pangram = "abcdefghijklmopqrstuvwxyz"
+    assert is_pangram(pangram) is False
