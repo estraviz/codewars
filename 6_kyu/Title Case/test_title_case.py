6e22a3b27266610d5083682565e031711c1a3dce
estraviz <javier.estraviz@gmail.com>
2019-12-13 00:39:25 +0100

    After refactor for kata: 'Title Case'

--- 6_kyu/Title Case/test_title_case.py
+++ 6_kyu/Title Case/test_title_case.py
@@ -7,3 +7,12 @@ def test_title_case():
     assert title_case('THE WIND IN THE WILLOWS',
                       'The In') == 'The Wind in the Willows'
     assert title_case('the quick brown fox') == 'The Quick Brown Fox'
+    assert title_case('', '') == ''
+    assert title_case('aBC deF Ghi', None) == 'Abc Def Ghi'
+    assert title_case('ab', 'ab') == 'Ab'
+    assert title_case('a bc', 'bc') == 'A bc'
+    assert title_case('a bc', 'BC') == 'A bc'
+    assert title_case('First a of in', 'an often into') == 'First A Of In'
+    assert title_case('a clash of KINGS', 'a an the OF') == 'A Clash of Kings'
+    assert title_case('the QUICK bRoWn fOX',
+                      'xyz fox quick the') == 'The quick Brown fox'
--- 6_kyu/Title Case/title_case.py
+++ 6_kyu/Title Case/title_case.py
@@ -3,12 +3,7 @@ Title Case
 """
 
 
-def title_case(title, minor_words=None):
-    if minor_words is None:
-        return ' '.join(word.title() for word in title.split())
-    output = []
-    for word in title.lower().split():
-        if len(output) == 0 or word not in minor_words.lower().split():
-            word = word.title()
-        output.append(word)
-    return ' '.join(output)
+def title_case(title, minor_words=''):
+    minor_words = '' if minor_words is None else minor_words.lower().split()
+    return ' '.join(word if word in minor_words else word.capitalize()
+                    for word in title.capitalize().split())
