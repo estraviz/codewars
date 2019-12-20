fd80343ad07baca36d9dd195e30a6acb3f77f529
estraviz <javier.estraviz@gmail.com>
2019-12-21 00:05:39 +0100

    After refactor for kata: 'Pirates... Are the Cannons..'

--- 8_kyu/Pirates.. Are the Cannons ready../cannons_ready.py
+++ 8_kyu/Pirates.. Are the Cannons ready../cannons_ready.py
@@ -4,6 +4,4 @@ Pirates!! Are the Cannons ready!??
 
 
 def cannons_ready(gunners):
-    return 'Fire!' if len(gunners) == sum(
-        1 for readiness in gunners.values()
-        if readiness == 'aye') else 'Shiver me timbers!'
+    return 'Shiver me timbers!' if 'nay' in gunners.values() else 'Fire!'
