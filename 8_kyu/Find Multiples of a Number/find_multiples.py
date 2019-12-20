ee5608956be5a56bc2257bb52dec8975f8c073a6
estraviz <javier.estraviz@gmail.com>
2019-12-20 23:43:19 +0100

    Adding refactor for kata: 'Find Multiples of a Number'

--- 8_kyu/Find Multiples of a Number/find_multiples.py
+++ 8_kyu/Find Multiples of a Number/find_multiples.py
@@ -4,6 +4,4 @@ Find Multiples of a Number
 
 
 def find_multiples(integer, limit):
-    return list(
-        filter(lambda x: x <= limit,
-               (integer * k for k in range(1, limit + 1))))
+    return list(range(integer, limit+1, integer))
