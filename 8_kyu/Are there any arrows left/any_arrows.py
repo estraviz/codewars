223ab9edd4e789c57e2e1e8238ed8543f2e61379
estraviz <javier.estraviz@gmail.com>
2019-12-22 20:13:20 +0100

    After refactor for kata: 'Are there any arrows left'

--- 8_kyu/Are there any arrows left/any_arrows.py
+++ 8_kyu/Are there any arrows left/any_arrows.py
@@ -4,5 +4,4 @@ Are there any arrows left?
 
 
 def any_arrows(arrows):
-    return True if any(('damaged' not in arrow or arrow['damaged'] is False)
-                       for arrow in arrows) else False
+    return not all(arrow.get('damaged', False) for arrow in arrows)
