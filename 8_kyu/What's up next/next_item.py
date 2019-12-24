f62e211c13cace43f457072ee7d3e2c68f68aba2
estraviz <javier.estraviz@gmail.com>
2019-12-24 01:41:54 +0100

    Adding refactor for kata: 'What's up next'

--- 8_kyu/What's up next/next_item.py
+++ 8_kyu/What's up next/next_item.py
@@ -4,14 +4,9 @@ What's up next?
 
 
 def next_item(xs, item):
-    if item in xs:
+    it = iter(xs)
+    if item in it:
         try:
-            try:
-                return xs[xs.index(item) + 1]
-            except IndexError:
-                pass
-        except AttributeError:
-            try:
-                return next(xs)
-            except StopIteration:
-                pass
+            return next(it)
+        except StopIteration:
+            pass
