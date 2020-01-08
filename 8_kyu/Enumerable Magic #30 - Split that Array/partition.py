97e5fa3ad6367791fb0fbc73d41410ebb62e5b93
estraviz <javier.estraviz@gmail.com>
2020-01-08 23:53:29 +0100

    After refactor (using filter) for kata: 'Enumerable Magic #30 - Split that Array!'

--- 8_kyu/Enumerable Magic #30 - Split that Array/partition.py
+++ 8_kyu/Enumerable Magic #30 - Split that Array/partition.py
@@ -4,10 +4,5 @@ Enumerable Magic #30 - Split that Array!
 
 
 def partition(arr, classifier_method):
-    first, second = [], []
-    for x in arr:
-        if classifier_method(x):
-            first.append(x)
-        else:
-            second.append(x)
-    return first, second
+    lst = list(filter(classifier_method, arr))
+    return lst, [item for item in arr if item not in lst]
