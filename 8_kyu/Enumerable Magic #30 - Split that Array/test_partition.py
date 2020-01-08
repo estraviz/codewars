6881cc61a09c6e75786d1875866032e18e6fc359
estraviz <javier.estraviz@gmail.com>
2020-01-08 23:32:38 +0100

    Adding more tests to kata: 'Enumerable Magic #30 - Split that Array!'

--- 8_kyu/Enumerable Magic #30 - Split that Array/test_partition.py
+++ 8_kyu/Enumerable Magic #30 - Split that Array/test_partition.py
@@ -1,7 +1,18 @@
 from partition import partition
 
 
-def test_partition():
+def test_partition_classifier_methods_asks_for_length():
     animals = ["cat", "dog", "duck", "cow", "donkey"]
     assert partition(animals, lambda x: len(x) == 3) == (['cat', 'dog', 'cow'],
                                                          ['duck', 'donkey'])
+
+
+def test_partition_classifier_methods_asks_for_a_first_character():
+    animals = ["cat", "dog", "duck", "cow", "donkey"]
+    assert partition(
+        animals, lambda x: x[0] == 'c') == (['cat',
+                                             'cow'], ['dog', 'duck', 'donkey'])
+
+
+def test_partition_entry_array_is_empty():
+    assert partition([], lambda x: len(x) == 3) == ([], [])
