64d7d0e43ce40f5fe9487c0ab4b32533812083d7
estraviz <javier.estraviz@gmail.com>
2020-01-09 01:34:20 +0100

    Adding refactor for kata: 'Farthest Distance'

--- 7_kyu/Farthest Distance/furthest_distance.py
+++ 7_kyu/Farthest Distance/furthest_distance.py
@@ -2,16 +2,13 @@
 Farthest Distance
 """
 
+from itertools import combinations
 from math import sqrt
 
 
 def furthest_distance(arr):
-    max_dist = 0
-    for x1 in arr:
-        for x2 in arr:
-            max_dist = max(max_dist, get_distance(x1, x2))
-    return round(max_dist, 2)
+    return round(max(dist(x, y) for [x, y] in list(combinations(arr, 2))), 2)
 
 
-def get_distance(x, y):
-    return sqrt((x[0] - y[0])**2 + (x[1] - y[1])**2)
+def dist(p, q):
+    return sqrt(sum((pi - qi)**2.0 for pi, qi in zip(p, q)))
