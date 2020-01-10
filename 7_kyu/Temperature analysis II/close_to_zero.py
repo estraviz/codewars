d8db2bbbf8bf205201fd43a5eac9729a4d62a189
estraviz <javier.estraviz@gmail.com>
2020-01-11 00:31:04 +0100

    Adding kata: 'Temperature analysis II'

--- 7_kyu/Temperature analysis II/close_to_zero.py
+++ 7_kyu/Temperature analysis II/close_to_zero.py
@@ -4,8 +4,5 @@ Temperature analysis II
 
 
 def close_to_zero(t):
-    if not t:
-        return 0
-    t = [int(x) for x in t.split()]
-    close = min(t, key=lambda x: abs(x))
-    return abs(close) if close < 0 and abs(close) in t else close
+    return 0 if not t else min([int(x) for x in t.split()],
+                               key=lambda x: (abs(x), -x))
