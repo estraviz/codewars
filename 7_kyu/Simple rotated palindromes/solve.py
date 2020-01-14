57e006cff89a686ed01e61e14d77bd51563af5f9
estraviz <javier.estraviz@gmail.com>
2020-01-14 20:56:58 +0100

    Adding refactor for kata: 'Simple rotated palindromes'

--- 7_kyu/Simple rotated palindromes/solve.py
+++ 7_kyu/Simple rotated palindromes/solve.py
@@ -6,6 +6,10 @@ Simple rotated palindromes
 def solve(st):
     for _ in st:
         st = st[-1] + st[:-1]
-        if st == st[::-1]:
+        if is_palindrome(st):
             return True
     return False
+
+
+def is_palindrome(s):
+    return s == s[::-1]
