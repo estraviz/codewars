e60882d24f0ce9e05f46d5dc49bd40677dda0f94
estraviz <javier.estraviz@gmail.com>
2020-01-19 20:00:20 +0100

    Adding kata: '8 towers'

---
+++ 7_kyu/8 towers/test_tower_combination.py
@@ -0,0 +1,6 @@
+from tower_combination import tower_combination
+
+
+def test_tower_combination():
+    assert tower_combination(2) == 2
+    assert tower_combination(3) == 6
---
+++ 7_kyu/8 towers/tower_combination.py
@@ -0,0 +1,9 @@
+"""
+8 towers
+"""
+
+from math import factorial
+
+
+def tower_combination(n):
+    return factorial(n)
