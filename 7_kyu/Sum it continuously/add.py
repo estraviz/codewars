86b8098125725bf8d53fa3eeceba6e2e7a952371
estraviz <javier.estraviz@gmail.com>
2020-01-21 00:49:41 +0100

    Adding refactor for kata: 'Sum it continuously'

--- 7_kyu/Sum it continuously/add.py
+++ 7_kyu/Sum it continuously/add.py
@@ -6,5 +6,5 @@ from itertools import accumulate
 
 
 def add(l):
-    return list(accumulate(l)) if isinstance(l, list) and all(
+    return [*accumulate(l)] if isinstance(l, list) and all(
         isinstance(num, int) for num in l) else 'Invalid input'
