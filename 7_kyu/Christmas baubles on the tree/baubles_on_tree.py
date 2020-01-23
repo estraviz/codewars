ae449267060b584f7bc2dfa0fb6bc98c756fd53b
estraviz <javier.estraviz@gmail.com>
2020-01-23 22:49:58 +0100

    Adding refactor for kata: 'Christmas baubles on the tree'

--- 7_kyu/Christmas baubles on the tree/baubles_on_tree.py
+++ 7_kyu/Christmas baubles on the tree/baubles_on_tree.py
@@ -7,10 +7,6 @@ def baubles_on_tree(baubles, branches):
     if branches == 0:
         return "Grandma, we will have to buy a Christmas tree first!"
     output = [baubles // branches] * branches
-    remainder = baubles % branches
-    branch_index = 0
-    while remainder > 0:
-        output[branch_index] += 1
-        remainder -= 1
-        branch_index += 1
+    for i in range(baubles % branches):
+        output[i] += 1
     return output
