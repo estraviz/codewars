3a01dd37145c21533905a9455d6ff628706a8913
estraviz <javier.estraviz@gmail.com>
2020-01-24 20:16:21 +0100

    Adding kata: 'Simple Fun #114: abacaba' and using lru_cache() from functools to speed up the calculation for big values of the input parameter

--- 7_kyu/Simple Fun #114: 'abacaba'/abacaba.py
+++ 7_kyu/Simple Fun #114: 'abacaba'/abacaba.py
@@ -3,8 +3,10 @@ Simple Fun #114: "abacaba"
 """
 
 from string import ascii_lowercase
+from functools import lru_cache
 
 
+@lru_cache(maxsize=None)
 def abacaba(k):
     str = ascii_lowercase[0]
     for i in range(1, 26):
