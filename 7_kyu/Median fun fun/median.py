10eeca12e503ed13bd20dc71cb11c981bc170749
estraviz <javier.estraviz@gmail.com>
2020-01-29 00:17:08 +0100

    Adding kata: 'Median fun fun'

--- 7_kyu/Median fun fun/median.py
+++ 7_kyu/Median fun fun/median.py
@@ -2,13 +2,8 @@
 Median fun fun
 """
 
+import statistics
+
 
 def median(arr):
-    arr = sorted(arr)
-    if len(arr) % 2 == 1:
-        mid = (len(arr) - 1) // 2
-        return arr[mid]
-    else:
-        mid1 = len(arr) // 2 - 1
-        mid2 = len(arr) // 2
-        return (arr[mid1] + arr[mid2]) / 2
+    return statistics.median(arr)
