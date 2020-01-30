0f4e5f1ea529ae2ab00e7517373b98025ec1c80e
estraviz <javier.estraviz@gmail.com>
2020-01-30 01:04:48 +0100

    Adding refactor for kata: 'Counter of neighbor ones', which uses groupby() from itertools

--- 7_kyu/Counter of neighbor ones/ones_counter.py
+++ 7_kyu/Counter of neighbor ones/ones_counter.py
@@ -2,20 +2,8 @@
 Counter of neighbor ones
 """
 
+from itertools import groupby
+
 
 def ones_counter(input):
-    output = []
-    count = 0
-    prev = 0
-    for num in input:
-        if num == 0:
-            if prev == 1:
-                output.append(count)
-                count = 0
-                prev = 0
-        else:
-            count += 1
-        prev = num
-    if count:
-        output.append(count)
-    return output
+    return [sum(group) for key, group in groupby(input) if key]
