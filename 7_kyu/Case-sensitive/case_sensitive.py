800720697c584c9ef06d0de3bbe91870544a646e
estraviz <javier.estraviz@gmail.com>
2020-02-08 11:31:41 +0100

    Adding refactor for kata: 'Case-sensitive'

--- 7_kyu/Case-sensitive/case_sensitive.py
+++ 7_kyu/Case-sensitive/case_sensitive.py
@@ -5,6 +5,5 @@ Case-sensitive!
 
 def case_sensitive(s):
     return [
-        True if len(s) == 0 or s.islower() else False,
-        [c for c in s if c.isupper()]
+        True if not s or s.islower() else False, [c for c in s if c.isupper()]
     ]
