e1f0d23538d782780aa94ea400f2011e184180d1
estraviz <javier.estraviz@gmail.com>
2020-03-19 12:28:06 +0100

    After refactor for kata: 'Zero Terminated Sum'. Added more tests

--- 7_kyu/Zero Terminated Sum/largest_sum.py
+++ 7_kyu/Zero Terminated Sum/largest_sum.py
@@ -4,9 +4,4 @@ Zero Terminated Sum
 
 
 def largest_sum(s):
-    max_sum = 0
-    for arr in s.split('0'):
-        aux_sum = sum(int(x) for x in arr)
-        if aux_sum > max_sum:
-            max_sum = aux_sum
-    return max_sum
+    return max(sum(int(x) for x in arr) for arr in s.split('0'))
--- 7_kyu/Zero Terminated Sum/test_largest_sum.py
+++ 7_kyu/Zero Terminated Sum/test_largest_sum.py
@@ -5,3 +5,10 @@ def test_sample():
     assert largest_sum("72102450111111090") == 11
     assert largest_sum("123004560") == 15
     assert largest_sum("0") == 0
+    assert largest_sum("4755843230") == 41
+    assert largest_sum("3388049750") == 25
+    assert largest_sum("3902864190") == 30
+    assert largest_sum("4098611970") == 41
+    assert largest_sum("5628206860") == 23
+    assert largest_sum("5891002970") == 23
+    assert largest_sum("0702175070") == 15
