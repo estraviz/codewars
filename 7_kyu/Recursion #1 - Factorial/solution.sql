c2f7fefc7a472dec2a70e78de9cc464ccd53020f
estraviz <javier.estraviz@gmail.com>
2020-04-13 00:47:57 +0200

    Adding sql kata: 'Recursion #1 - Factorial'

---
+++ 7_kyu/Recursion #1 - Factorial/solution.sql
@@ -0,0 +1,18 @@
+-- Recursion #1 - Factorial: Create recursively the table with n up to and including 16
+-- PostgreSQL 9.6
+WITH RECURSIVE factorial (n, fact) AS (
+  SELECT
+    0,
+    CAST(1 AS BIGINT)
+  UNION ALL
+  SELECT
+    n + 1,
+    (n + 1) * fact
+  FROM factorial
+  WHERE
+    n < 16
+)
+SELECT
+  n,
+  fact
+FROM factorial;
