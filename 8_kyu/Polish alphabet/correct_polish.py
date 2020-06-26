a9876221262dbd67f1b086ba3a8b656bd27f89d6
estraviz <javier.estraviz@gmail.com>
2020-06-27 00:48:07 +0200

    Adding refactor to solution for kata: 'Polish alphabet', now using maketrans and translate methods from str

--- 8_kyu/Polish alphabet/correct_polish.py
+++ 8_kyu/Polish alphabet/correct_polish.py
@@ -4,15 +4,4 @@ Polish alphabet
 
 
 def correct_polish_letters(st):
-    diacritics = {
-        'ą': 'a',
-        'ć': 'c',
-        'ę': 'e',
-        'ł': 'l',
-        'ń': 'n',
-        'ó': 'o',
-        'ś': 's',
-        'ź': 'z',
-        'ż': 'z'
-    }
-    return ''.join([diacritics.get(chr, chr) for chr in st])
+    return st.translate(str.maketrans("ąćęłńóśźż", "acelnoszz"))
