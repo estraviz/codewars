377990f138575fad98a519da8c15d709d28b2884
estraviz <javier.estraviz@gmail.com>
2020-08-07 23:45:56 +0200

    Adding refactor for kata 'Invert The Triangle'

--- 7_kyu/Invert The Triangle/invert_triangle.py
+++ 7_kyu/Invert The Triangle/invert_triangle.py
@@ -4,12 +4,5 @@ Invert The Triangle
 
 
 def invert_triangle(t):
-    lst = []
-    for c in t:
-        if c == " ":
-            lst.append("#")
-        elif c == "#":
-            lst.append(" ")
-        else:
-            lst.append(c)
+    lst = [{" ": "#", "#": " "}.get(c, c) for c in t]
     return ''.join(x for x in lst[::-1])
