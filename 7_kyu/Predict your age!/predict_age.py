86af5638ed90bd013c0418e3bb2a55e2e9ae3da1
estraviz <javier.estraviz@gmail.com>
2020-08-09 23:53:25 +0200

    Refactor solution for kata 'Predict your age!'

--- 7_kyu/Predict your age!/predict_age.py
+++ 7_kyu/Predict your age!/predict_age.py
@@ -4,6 +4,5 @@
 from math import sqrt
 
 
-def predict_age(age_1, age_2, age_3, age_4, age_5, age_6, age_7, age_8):
-    ages = [age_1, age_2, age_3, age_4, age_5, age_6, age_7, age_8]
+def predict_age(*ages):
     return int(sqrt(sum(map(lambda x: x**2, ages)))/2)
--- 7_kyu/Predict your age!/test_predict_age.py
+++ 7_kyu/Predict your age!/test_predict_age.py
@@ -3,3 +3,4 @@ from predict_age import predict_age
 
 def test_predict_age():
     assert predict_age(65, 60, 75, 55, 60, 63, 64, 45) == 86
+    assert predict_age(32, 54, 76, 65, 34, 63, 64, 45) == 79
