55afd6bae3a8d0d845148ad659658311d6639120
estraviz <javier.estraviz@gmail.com>
2020-08-12 23:53:29 +0200

    After refactor for kata: 'Simple Fun #87: Shuffled Array'

--- 7_kyu/Simple Fun #87: Shuffled Array/shuffled_array.py
+++ 7_kyu/Simple Fun #87: Shuffled Array/shuffled_array.py
@@ -4,6 +4,6 @@ Simple Fun #87: Shuffled Array
 
 
 def shuffled_array(s):
-    for i, val in enumerate(s):
-        if sum(s[:i] + s[i+1:]) == val:
-            return sorted(s[:i] + s[i+1:])
+    s.remove(sum(s)//2)
+    return sorted(s)
+
