c8b92d2d73a508fca17e734a510d5d202391789f
estraviz <javier.estraviz@gmail.com>
2020-09-06 23:11:31 +0200

    Improved solution for kata 'Split By Value' by using sorted and key condition

--- 7_kyu/Split By Value/solution.py
+++ 7_kyu/Split By Value/solution.py
@@ -3,11 +3,4 @@
 
 
 def split_by_value(k, elements):
-    before_k = []
-    after_k = []
-    for num in elements:
-        if num < k:
-            before_k.append(num)
-        else:
-            after_k.append(num)
-    return before_k + after_k
+    return sorted(elements, key=lambda x: x < k, reverse=True)
