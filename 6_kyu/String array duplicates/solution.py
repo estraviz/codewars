5884a4f630835942967bc84ee07cbf6803c0af3d
estraviz <javier.estraviz@gmail.com>
2020-09-15 23:21:45 +0200

    Adding refactorized solution for kata: 'String array duplicates' by using itertools.groupby()

--- 6_kyu/String array duplicates/solution.py
+++ 6_kyu/String array duplicates/solution.py
@@ -1,17 +1,8 @@
 """String array duplicates
 """
 
+import itertools as it
+
 
 def dup(arr):
-    prev_chr = ''
-    clean_word = ""
-    output = []
-    for word in arr:
-        for chr in word:
-            if chr != prev_chr:
-                clean_word += chr
-            prev_chr = chr
-        output.append(clean_word)
-        clean_word = ""
-        prev_chr = ''
-    return output
+    return ["".join(chr for chr, group in it.groupby(word)) for word in arr]
