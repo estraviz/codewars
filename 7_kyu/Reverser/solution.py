72662655d2f505e3f61018eb4b2813f03e7c2b45
estraviz <javier.estraviz@gmail.com>
2020-11-02 00:24:26 +0100

    Adding refactored solution. Adding test to verify that forbidden words are not part of the solution

--- 7_kyu/Reverser/solution.py
+++ 7_kyu/Reverser/solution.py
@@ -1,14 +1,8 @@
-"""Reverser
-"""
-
-
 def reverse(n):
-    """Returns n with all digits reversed. Assume positive n."""
     i = 0
     coefs = []
     while n > 0:
-        rem = n % 10
-        n = n // 10
-        coefs.append(rem)
+        coefs.append(n % 10)
+        n //= 10
         i += 1
-    return sum(10**i * k for i, k in enumerate(coefs[::-1]))
+    return sum(10 ** i * k for i, k in enumerate(coefs[::-1]))
--- 7_kyu/Reverser/test_solution.py
+++ 7_kyu/Reverser/test_solution.py
@@ -1,8 +1,47 @@
+import pytest
 from solution import reverse
 
 
-def test_reverse():
-    assert reverse(321) == 123
-    assert reverse(1234) == 4321
-    assert reverse(10987) == 78901
-    assert reverse(1020) == 201
+@pytest.mark.parametrize(
+    "n, reversed",
+    [[321, 123], [1234, 4321], [10987, 78901], [1020, 201], [12005000, 50021]],
+)
+def test_reverse(n, reversed):
+    assert reverse(n) == reversed
+
+
+words = [
+    "encode",
+    "decode",
+    "join",
+    "zfill",
+    "codecs",
+    "chr",
+    "bytes",
+    "ascii",
+    "substitute",
+    "template",
+    "bin",
+    "os",
+    "sys",
+    "re ",
+    '"',
+    "'",
+    "str",
+    "repr",
+    "%s",
+    "format",
+    "type",
+    "__",
+    ".keys",
+    "eval",
+    "exec",
+    "subprocess",
+]
+
+
+def test_does_not_include_words_in_list():
+    with open("solution.py", "r") as f:
+        text = f.read()
+        for word in words:
+            assert not (word in text)
