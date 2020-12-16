652f9766564cb807d0aa7334d4f34b897f464c3a
estraviz <javier.estraviz@gmail.com>
2020-12-17 00:19:04 +0100

    Adding more tests for kata: 'Help the bookseller !'

--- 6_kyu/Help the bookseller !/python/test_solution.py
+++ 6_kyu/Help the bookseller !/python/test_solution.py
@@ -1,7 +1,43 @@
+import pytest
+
 from solution import stock_list
 
 
-def test_stock_list():
-    list_of_art = ["ABAR 200", "CDXE 500", "BKWR 250", "BTSQ 890", "DRTY 600"]
-    list_of_cat = ["A", "B"]
-    assert stock_list(list_of_art, list_of_cat) == "(A : 200) - (B : 1140)"
+@pytest.mark.parametrize(
+    "list_of_art, list_of_cat, result",
+    [
+        (
+            ["ABAR 200", "CDXE 500", "BKWR 250", "BTSQ 890", "DRTY 600"],
+            ["A", "B"],
+            "(A : 200) - (B : 1140)",
+        ),
+        (
+            ["BBAR 150", "CDXE 515", "BKWR 250", "BTSQ 890", "DRTY 600"],
+            ["A", "B", "C", "D"],
+            "(A : 0) - (B : 1290) - (C : 515) - (D : 600)",
+        ),
+        (
+            ["CBART 20", "CDXEF 50", "BKWRK 25", "BTSQZ 89", "DRTYM 60"],
+            ["A", "B", "C", "W"],
+            "(A : 0) - (B : 114) - (C : 70) - (W : 0)",
+        ),
+        (
+            ["ROXANNE 102", "RHODODE 123", "BKWRKAA 125", "BTSQZFG 239", "DRTYMKH 060"],
+            ["B", "R", "D", "X"],
+            "(B : 364) - (R : 225) - (D : 60) - (X : 0)",
+        ),
+        ([], ["B", "R", "D", "X"], ""),
+        (
+            ["ROXANNE 102", "RHODODE 123", "BKWRKAA 125", "BTSQZFG 239", "DRTYMKH 060"],
+            [],
+            "",
+        ),
+        (
+            ["ROXANNE 102", "RHODODE 123", "BKWRKAA 125", "BTSQZFG 239", "DRTYMKH 060"],
+            ["U", "V", "R"],
+            "(U : 0) - (V : 0) - (R : 225)",
+        ),
+    ],
+)
+def test_stock_list(list_of_art, list_of_cat, result):
+    assert stock_list(list_of_art, list_of_cat) == result
