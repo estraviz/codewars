56be3e09419288bdf111fcda05eddef7e1f3e2fe
estraviz <javier.estraviz@gmail.com>
2021-02-02 23:58:10 +0100

    Adding refactored solution plus more tests for kata: 'Function composition'

--- 6_kyu/Function Composition/python/solution.py
+++ 6_kyu/Function Composition/python/solution.py
@@ -1,11 +1,5 @@
 """Function Composition"""
 
 
-from functools import reduce
-
-
-def compose(*funcs):
-    def compose_all_elems(f, g):
-        return lambda *x: f(g(*x))
-
-    return reduce(compose_all_elems, funcs)
+def compose(f, g):
+    return lambda *x: f(g(*x))
--- 6_kyu/Function Composition/python/test_solution.py
+++ 6_kyu/Function Composition/python/test_solution.py
@@ -2,6 +2,15 @@ from solution import compose
 
 
 def test_compose():
-    f = lambda a: a + 1
-    g = lambda a: a
-    assert compose(f, g)(0) == 1
+    add1 = lambda a: a + 1
+    min1 = lambda a: a - 1
+    div2 = lambda a: a / 2
+    add15 = lambda a: a + 15
+    this = lambda a: a
+    total = lambda *args: sum(args)
+
+    assert compose(add1, this)(0) == 1
+    assert compose(add1, div2)(2) == 2
+    assert compose(add1, total)(1, 2, 3, 4, 5) == 16
+    assert compose(add15, this)(0) == 15
+    assert compose(this, add15)(0) == 15
