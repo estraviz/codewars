563a020e0b3b2c1d4f3bbf964d47e0f3d390c63a
estraviz <javier.estraviz@gmail.com>
2021-03-16 21:37:54 +0100

    Adding more tests for kata: 'Bouncy Numbers'

--- 7_kyu/Bouncy Numbers/python/test_solution.py
+++ 7_kyu/Bouncy Numbers/python/test_solution.py
@@ -3,7 +3,7 @@ import pytest
 from solution import is_bouncy
 
 
-data = [
+data_small_numbers = [
     (0, False),
     (99, False),
     (101, True),
@@ -14,7 +14,24 @@ data = [
 
 
 @pytest.mark.parametrize(
-    "number, result", data
+    "number, result", data_small_numbers
+)
+def test_is_bouncy(number, result):
+    assert is_bouncy(number) == result
+
+
+data_large_numbers = [
+    (2379, False),
+    (29340, True),
+    (234689, False),
+    (98874, False),
+    (92722983, True),
+    (129347924210, True),
+]
+
+
+@pytest.mark.parametrize(
+    "number, result", data_large_numbers
 )
 def test_is_bouncy(number, result):
     assert is_bouncy(number) == result
