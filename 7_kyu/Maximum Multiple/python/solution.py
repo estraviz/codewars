2c16728c56a97426a51955df2ee7a95e665534fb
estraviz <javier.estraviz@gmail.com>
2021-03-20 00:06:52 +0100

    After refactoring solution for kata: 'Maximum Multiple'

--- 7_kyu/Maximum Multiple/python/solution.py
+++ 7_kyu/Maximum Multiple/python/solution.py
@@ -2,7 +2,4 @@
 
 
 def max_multiple(divisor, bound):
-    while bound >= divisor:
-        if bound % divisor == 0:
-            return bound
-        bound -= 1
+    return bound - bound % divisor
