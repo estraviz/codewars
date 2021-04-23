2db603c76da68c750249b5fdfb22fd5444e71076
estraviz <javier.estraviz@gmail.com>
2021-04-24 00:47:17 +0200

    After refactor, using now itertools groupby function

--- 6_kyu/Adjacent repeated words in a string/python/solution.py
+++ 6_kyu/Adjacent repeated words in a string/python/solution.py
@@ -1,18 +1,7 @@
 """Adjacent repeated words in a string"""
 
+from itertools import groupby
+
 
 def count_adjacent_pairs(st):
-    adjacents = 0
-    prev_word = ""
-    new_group = False
-
-    for word in st.split(" "):
-        word = word.lower()
-        if word != prev_word:
-            new_group = True
-            prev_word = word
-        else:
-            if new_group:
-                adjacents += 1
-                new_group = False
-    return adjacents
+    return sum(1 for _, g in groupby(st.lower().split()) if len(list(g)) > 1)
