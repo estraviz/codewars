fa19c36417e2e3e4c721110367cda91dd35d5ff5
estraviz <javier.estraviz@gmail.com>
2021-04-30 19:56:40 +0200

    Added refactored and simpler solution for kata: 'Number Of Occurrences'

--- 7_kyu/Number Of Occurrences/python/solution.py
+++ 7_kyu/Number Of Occurrences/python/solution.py
@@ -1,7 +1,5 @@
 """Number Of Occurrences"""
 
-from collections import Counter
-
 
 def number_of_occurrences(element, sample):
-    return Counter(sample).get(element, 0)
+    return sample.count(element)
