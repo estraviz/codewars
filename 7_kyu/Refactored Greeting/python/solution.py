91c7efa440548a10551a09f032f0487dae35a8d6
estraviz <javier.estraviz@gmail.com>
2021-05-08 23:23:59 +0200

    Added a simpler solution for kata: 'Refactored Greeting'

--- 7_kyu/Refactored Greeting/python/solution.py
+++ 7_kyu/Refactored Greeting/python/solution.py
@@ -3,11 +3,7 @@
 
 class Person:
     def __init__(self, name):
-        self._name = name
-
-    @property
-    def name(self):
-        return self._name
+        self.name = name
 
     def greet(self, friend):
-        return "Hello %s, my name is %s" % (Person(friend).name, self._name)
+        return f"Hello {friend}, my name is {self.name}"
