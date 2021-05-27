0917304fcd91c4a2bcbf0e2a78d55f1902ad0487
estraviz <javier.estraviz@gmail.com>
2021-05-27 21:26:58 +0200

    Added refactored solution

--- 7_kyu/Maximum Product/python/solution.py
+++ 7_kyu/Maximum Product/python/solution.py
@@ -2,7 +2,4 @@
 
 
 def adjacent_element_product(array):
-    max_prod = None
-    for x, y in zip(array, array[1:]):
-        max_prod = x * y if not max_prod else max(max_prod, x * y)
-    return max_prod
+    return max(x * y for x, y in zip(array, array[1:]))
