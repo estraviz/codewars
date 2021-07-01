dd46c09f2ace042af72c00b56c768df20434ec6f
estraviz <javier.estraviz@gmail.com>
2021-07-01 23:51:57 +0200

    Added a second solution for kata: 'Difference between two collections'

--- 7_kyu/Difference between two collections/python/solution.py
+++ 7_kyu/Difference between two collections/python/solution.py
@@ -2,8 +2,4 @@
 
 
 def diff(a, b):
-    return sorted(set(not_in(a, b) + not_in(b, a)))
-
-
-def not_in(a, b):
-    return [x for x in a if x not in b]
+    return sorted(set(a).symmetric_difference(b))
