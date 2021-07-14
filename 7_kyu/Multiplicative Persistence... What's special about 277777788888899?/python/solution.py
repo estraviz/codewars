141a489c7ac64c56f036bab2aa2889e57da66d2c
estraviz <javier.estraviz@gmail.com>
2021-07-15 00:04:09 +0200

    Added refactored solution for kata: 'Multiplicative Persistence... What's special about 277777788888899?'

--- 7_kyu/Multiplicative Persistence... What's special about 277777788888899?/python/solution.py
+++ 7_kyu/Multiplicative Persistence... What's special about 277777788888899?/python/solution.py
@@ -1,22 +1,15 @@
 """Multiplicative Persistence... What's special about 277777788888899?"""
 
 from functools import reduce
+from operator import mul
 
 
 def per(n):
     lst = []
-    while is_not_single_digit(n):
-        lst.append(n := get_reduced_next(n))
+    while not is_single_digit(n):
+        lst.append(n := reduce(mul, map(int, str(n))))
     return lst
 
 
-def is_not_single_digit(n):
-    return n >= 10
-
-
-def get_reduced_next(n):
-    return reduce((lambda x, y: x * y), get_list_of_digits(n))
-
-
-def get_list_of_digits(n):
-    return list(int(x) for x in list(str(n)))
+def is_single_digit(n):
+    return n <= 9
