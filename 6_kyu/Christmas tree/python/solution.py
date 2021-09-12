435cc56cb7f7c3424a900ad6b05f11fa42400837
estraviz <javier.estraviz@gmail.com>
2021-09-12 15:57:01 +0200

    Added refactored solution for kata: 'Christmas tree'

--- 6_kyu/Christmas tree/python/solution.py
+++ 6_kyu/Christmas tree/python/solution.py
@@ -2,4 +2,8 @@
 
 
 def christmas_tree(height):
-    return "\n".join(" " * (height - h) + '*' * (2*h - 1) + " " * (height - h) for h in range(1, height + 1))
+    return "\n".join(get_row(height, h) for h in range(1, height + 1))
+
+
+def get_row(height, h):
+    return " " * (height - h) + '*' * (2*h - 1) + " " * (height - h)
