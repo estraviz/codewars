bf28c733aa0c1d9c4ad95039363d27974be6e801
estraviz <javier.estraviz@gmail.com>
2021-10-03 17:38:55 +0200

    Added improved solution for kata: 'String Letter Counting'

--- 6_kyu/String Letter Counting/python/solution.py
+++ 6_kyu/String Letter Counting/python/solution.py
@@ -1,13 +1,10 @@
 """String Letter Counting"""
 
-from collections import defaultdict
+from collections import Counter
 from collections import OrderedDict
 
 
 def string_letter_count(s):
-    counts = defaultdict(int)
-    for c in s.lower():
-        if c.isalpha():
-            counts[c] += 1
+    counts = Counter(filter(str.isalpha, s.lower()))
     ordered_counts = OrderedDict(sorted(counts.items()))
     return "".join(str(v) + k for k, v in ordered_counts.items())
