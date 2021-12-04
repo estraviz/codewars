7a447a6bbf6be695fa23ff3f6501b7458a9b5541
estraviz <javier.estraviz@gmail.com>
2021-12-04 21:30:05 +0100

    Refactored python solution for kata: 'Not very secure'

--- 5_kyu/Not very secure/python/solution.py
+++ 5_kyu/Not very secure/python/solution.py
@@ -1,11 +1,3 @@
 # Not very secure
-from string import ascii_letters
-from string import digits
-
-
 def alphanumeric(password):
-    return password is not "" and \
-            all(
-                True if c in ascii_letters or c in digits else False
-                for c in password
-                )
+    return password.isalnum()
