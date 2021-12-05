e43a3c16adcd1a8f81fb189187bad27ca8c327f4
estraviz <javier.estraviz@gmail.com>
2021-12-05 21:44:30 +0100

    Adapted solution to Python 3.9

--- 5_kyu/Common Denominators/python/solution.py
+++ 5_kyu/Common Denominators/python/solution.py
@@ -1,11 +1,10 @@
 # Common Denominators
-from functools import reduce
-from math import gcd
+import math
 
 
 def convert_fracts(lst):
     converted_fracts = []
     if lst:
-        lcm = reduce(lambda a, b: a * b // gcd(a, b), [d for _, d in lst])
+        lcm = math.lcm(*[d for _, d in lst])
         converted_fracts = [[num * lcm // den, lcm] for num, den in lst]
     return converted_fracts
