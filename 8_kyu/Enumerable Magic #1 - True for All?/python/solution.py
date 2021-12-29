334e7ef27aa5cea5ca7d0c335ed49b5bf89b7c61
estraviz <javier.estraviz@gmail.com>
2021-12-30 00:05:16 +0100

    Solved sintaxis error

--- 8_kyu/Enumerable Magic #1 - True for All?/python/solution.py
+++ 8_kyu/Enumerable Magic #1 - True for All?/python/solution.py
@@ -1,6 +1,6 @@
 # Enumerable Magic #1 - True for All?
 def _all(seq, fun):
     for x in seq:
-        if not x in filter(fun, seq):
+        if x not in filter(fun, seq):
             return False
     return True
