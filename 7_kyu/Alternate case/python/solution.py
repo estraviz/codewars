f5b749783a0d404861c8ebf0e7c079c83c16ad02
estraviz <javier.estraviz@gmail.com>
2022-01-01 09:52:18 +0100

    Added additional test cases. Simplified solution

--- 7_kyu/Alternate case/python/solution.py
+++ 7_kyu/Alternate case/python/solution.py
@@ -1,7 +1,3 @@
 # Alternate case
 def alternate_case(s):
-    return "".join(
-        chr(
-            ord(c) + 32 * (1 if c.isupper() else -1 if c.islower() else 0)
-        ) for c in s
-    )
+    return s.swapcase()
--- 7_kyu/Alternate case/python/test_solution.py
+++ 7_kyu/Alternate case/python/test_solution.py
@@ -18,3 +18,18 @@ basic_tests = [
 )
 def test_should_alternate_case_for_basic_tests(s, expected):
     assert alternate_case(s) == expected
+
+
+additional_tests = [
+    ("HuMpTy DuMpTy SaT On A WaLl", "hUmPtY dUmPtY sAt oN a wAlL"),
+    ("aBracaDabRa", "AbRACAdABrA"),
+    ("Hickory DICKORY dock", "hICKORY dickory DOCK"),
+    ("Jack JUMPED over THE CaNdLeStIcK", "jACK jumped OVER the cAnDlEsTiCk"),
+]
+
+
+@pytest.mark.parametrize(
+    "s, expected", additional_tests
+)
+def test_should_alternate_case_for_additional_tests(s, expected):
+    assert alternate_case(s) == expected
