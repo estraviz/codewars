9f5b6d74555ba80a2411fd815758ec7a44d9c51d
estraviz <javier.estraviz@gmail.com>
2022-01-05 23:22:27 +0100

    Restored first solution

--- 7_kyu/Coprime Validator/python/solution.py
+++ 7_kyu/Coprime Validator/python/solution.py
@@ -1,6 +1,11 @@
 # Coprime Validator
-from math import gcd
-
-
 def are_coprime(n, m):
-    return gcd(n, m) == 1
+    return max(get_shared(get_fact(n), get_fact(m))) == 1
+
+
+def get_fact(k):
+    return [i for i in range(1, k+1) if k % i == 0]
+
+
+def get_shared(l1, l2):
+    return list(set(l1) & set(l2))
