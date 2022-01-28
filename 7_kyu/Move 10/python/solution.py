2f18b97b74d2ac52695633c61b6ed46b59ff9ce5
estraviz <javier.estraviz@gmail.com>
2022-01-28 23:05:19 +0100

    Added another python solution to kata 'Move 10'

--- 7_kyu/Move 10/python/solution.py
+++ 7_kyu/Move 10/python/solution.py
@@ -1,8 +1,7 @@
 # Move 10
 def move_ten(st):
-    new_st = ""
-    for x in st:
-        if (code := ord(x) + 10) > ord('z'):
-            code = code % ord('z') + ord('a') - 1
-        new_st += chr(code)
-    return new_st
+    return "".join(
+        chr(
+            ord('a') + (ord(c) - ord('a') + 10) % (ord('z') - ord('a') + 1)
+        ) for c in st
+    )
