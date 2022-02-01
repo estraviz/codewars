cf8b5fedb5db6b1d276d2c09286d83828b8fc2c8
estraviz <javier.estraviz@gmail.com>
2022-02-01 22:11:57 +0100

    Added more tests

--- 7_kyu/Coding Meetup #5 - Higher-Order Functions Series - Prepare the count of languages/python/test_solution.py
+++ 7_kyu/Coding Meetup #5 - Higher-Order Functions Series - Prepare the count of languages/python/test_solution.py
@@ -1,7 +1,7 @@
 from solution import count_languages
 
 
-def test_example_case():
+def test_example_case_1():
     list1 = [
         {
             'firstName': 'Noah', 'lastName': 'M.', 'country': 'Switzerland',
@@ -20,5 +20,80 @@ def test_example_case():
             'continent': 'Europe', 'age': 81, 'language': 'C'
         },
     ]
-    answer = {'C': 2, 'JavaScript': 1, 'Ruby': 1}
-    assert count_languages(list1) == answer
+    answer1 = {'C': 2, 'JavaScript': 1, 'Ruby': 1}
+    assert count_languages(list1) == answer1
+
+
+def test_example_case_2():
+    list2 = [
+        {
+            'firstName': 'Kseniya', 'lastName': 'T.', 'country': 'Belarus',
+            'continent': 'Europe', 'age': 29, 'language': 'JavaScript'
+        },
+        {
+            'firstName': 'Amar', 'lastName': 'V.',
+            'country': 'Bosnia and Herzegovina', 'continent': 'Europe',
+            'age': 32, 'language': 'Ruby'
+        },
+    ]
+    answer2 = {'Ruby': 1, 'JavaScript': 1}
+    assert count_languages(list2) == answer2
+
+
+def test_example_case_3():
+    list3 = [
+        {
+            'firstName': 'Sofia', 'lastName': 'P.', 'country': 'Italy',
+            'continent': 'Europe', 'age': 41, 'language': 'Clojure'
+        },
+        {
+            'firstName': 'Jayden', 'lastName': 'P.', 'country': 'Jamaica',
+            'continent': 'Americas', 'age': 42, 'language': 'JavaScript'
+        },
+        {
+            'firstName': 'Sou', 'lastName': 'B.', 'country': 'Japan',
+            'continent': 'Asia', 'age': 43, 'language': 'Python'
+        },
+        {
+            'firstName': 'Rimas', 'lastName': 'C.', 'country': 'Jordan',
+            'continent': 'Asia', 'age': 44, 'language': 'Java'
+        }
+    ]
+    answer3 = {'Clojure': 1, 'JavaScript': 1, 'Python': 1, 'Java': 1}
+    assert count_languages(list3) == answer3
+
+
+def test_example_case_4():
+    list4 = [
+        { 'firstName': 'Alexander', 'lastName': 'F.', 'country': 'Russia', 'continent': 'Europe', 'age': 89, 'language': 'Java' },
+        { 'firstName': 'Fatima', 'lastName': 'K.', 'country': 'Saudi Arabia', 'continent': 'Asia', 'age': 21, 'language': 'Clojure' },
+        { 'firstName': 'Mark', 'lastName': 'G.', 'country': 'Scotland', 'continent': 'Europe', 'age': 22, 'language': 'JavaScript' },
+        { 'firstName': 'Nikola', 'lastName': 'H.', 'country': 'Serbia', 'continent': 'Europe', 'age': 29, 'language': 'Python' },
+        { 'firstName': 'Jakub', 'lastName': 'I.', 'country': 'Slovakia', 'continent': 'Europe', 'age': 28, 'language': 'Java' },
+        { 'firstName': 'Luka', 'lastName': 'J.', 'country': 'Slovenia', 'continent': 'Europe', 'age': 29, 'language': 'Clojure' }
+        ]
+    answer4 = { 'Clojure': 2, 'JavaScript': 1, 'Python': 1, 'Java': 2 }
+    assert count_languages(list4) == answer4
+
+
+def test_example_case_5():
+    list5 = [
+        {
+            'firstName': 'Sumayah', 'lastName': 'M.', 'country': 'Tajikistan',
+            'continent': 'Asia', 'age': 30, 'language': 'Ruby'
+        },
+        {
+            'firstName': 'Mehdi', 'lastName': 'H.', 'country': 'Tunisia',
+            'continent': 'Africa', 'age': 42, 'language': 'Python'
+        },
+        {
+            'firstName': 'Yusuf', 'lastName': 'N.', 'country': 'Turkey',
+            'continent': 'Europe', 'age': 22, 'language': 'Python'
+        },
+        {
+            'firstName': 'Artem', 'lastName': 'O.', 'country': 'Ukraine',
+            'continent': 'Europe', 'age': 29, 'language': 'Java'
+        }
+    ]
+    answer5 = {'Ruby': 1, 'Java': 1, 'Python': 2}
+    assert count_languages(list5) == answer5
