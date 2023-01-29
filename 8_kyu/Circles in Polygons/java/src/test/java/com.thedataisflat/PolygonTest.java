2f61970f199a12b337553fb182e6556e1e10c9fb
estraviz <javier.estraviz@gmail.com>
2023-01-29 01:01:17 +0100

    Format

--- 8_kyu/Circles in Polygons/java/src/test/java/com.thedataisflat/PolygonTest.java
+++ 8_kyu/Circles in Polygons/java/src/test/java/com.thedataisflat/PolygonTest.java
@@ -6,23 +6,23 @@ import org.junit.Test;
 public class PolygonTest {
 
     @Test
-    public void test1(){
-        //Square with sides of 5 units
-        Polygon poly=new Polygon(4, 5);
+    public void test1() {
+        // Square with sides of 5 units
+        Polygon poly = new Polygon(4, 5);
         assertEquals("5.000", String.format("%.3f", poly.circleDiameter()));
     }
 
     @Test
     public void test2() {
-        //Octogon with sides of 9 units
-        Polygon poly=new Polygon(8, 9);
+        // Octogon with sides of 9 units
+        Polygon poly = new Polygon(8, 9);
         assertEquals("21.728", String.format("%.3f", poly.circleDiameter()));
     }
 
     @Test
     public void test3() {
-        //Triangle with sides of 4 units
-        Polygon poly=new Polygon(3, 4);
+        // Triangle with sides of 4 units
+        Polygon poly = new Polygon(3, 4);
         assertEquals("2.309", String.format("%.3f", poly.circleDiameter()));
     }
 }
