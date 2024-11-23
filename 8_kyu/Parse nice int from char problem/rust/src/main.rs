b7893fdc8441512596321616e811315f119e568d
Javier Estraviz <javier.estraviz@gmail.com>
2024-11-23 19:39:06 +0100

    Added Rust solution to kata 'Parse nice int from char problem (8kyu)'

---
+++ 8_kyu/Parse nice int from char problem/rust/src/main.rs
@@ -0,0 +1,16 @@
+fn get_age(age: &str) -> u32 {
+    age.chars()
+        .nth(0)
+        .unwrap()
+        .to_string()
+        .parse()
+        .expect("Not a valid number")
+}
+
+#[test]
+fn basic_tests() {
+    assert_eq!(get_age("2 years old"), 2);
+    assert_eq!(get_age("4 years old"), 4);
+    assert_eq!(get_age("5 years old"), 5);
+    assert_eq!(get_age("7 years old"), 7);
+}
