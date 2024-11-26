f95b732417d26c9c3a757a221bd34f9ca0e65dd7
Javier Estraviz <javier.estraviz@gmail.com>
2024-11-26 21:46:42 +0100

    Added Rust solution to kata 'Is this my tail? (8kyu)'

---
+++ 8_kyu/Is this my tail/rust/src/main.rs
@@ -0,0 +1,18 @@
+fn correct_tail(body: &str, tail: char) -> bool {
+    body.chars().nth(body.chars().count() - 1) == Some(tail)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::correct_tail;
+
+    #[test]
+    fn fixed_tests() {
+        assert_eq!(correct_tail("Fox", 'x'), true);
+        assert_eq!(correct_tail("Rhino", 'o'), true);
+        assert_eq!(correct_tail("Meerkat", 't'), true);
+        assert_eq!(correct_tail("Emu", 't'), false);
+        assert_eq!(correct_tail("Badger", 's'), false);
+        assert_eq!(correct_tail("Giraffe", 'd'), false);
+    }
+}
