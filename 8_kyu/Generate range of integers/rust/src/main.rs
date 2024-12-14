ab40af4245ce75fa3e066266571b28f9b08052eb
Javier Estraviz <javier.estraviz@gmail.com>
2024-12-14 01:13:52 +0100

    Added Rust solution to kata 'Generate range of integers (8kyu)'

---
+++ 8_kyu/Generate range of integers/rust/src/main.rs
@@ -0,0 +1,23 @@
+fn generate_range(min: usize, max: usize, step: usize) -> Vec<usize> {
+    let mut v: Vec<usize> = [].to_vec();
+
+    for x in (min..=max).step_by(step) {
+        v.push(x);
+    }
+
+    v
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_basic_cases() {
+        assert_eq!(generate_range(2, 10, 2), [2, 4, 6, 8, 10]);
+        assert_eq!(generate_range(1, 10, 3), [1, 4, 7, 10]);
+        assert_eq!(generate_range(1, 10, 1), [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
+        assert_eq!(generate_range(1, 10, 4), [1, 5, 9]);
+        assert_eq!(generate_range(1, 10, 5), [1, 6]);
+    }
+}
