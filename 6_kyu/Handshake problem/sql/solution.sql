fad5bea6315b1c121d42fcc56e6c67c1a41e17bb
Javier Estraviz <javier.estraviz@gmail.com>
2024-12-19 01:17:07 +0100

    Added refactor to SQL solution for kata 'Handshake problem (6kyu)'

--- 6_kyu/Handshake problem/sql/solution.sql
+++ 6_kyu/Handshake problem/sql/solution.sql
@@ -5,9 +5,9 @@
 -- n = res*(res-1)/ 2 -> res = (1 + sqrt(8*n + 1))/2
 select n,
   case
-    when n = 0 then 0
-    when n = 1 then 2
-    when n = 2 then 3
-    else cast(ceiling((1 + sqrt(8*n + 1))/2) as int)
+    when n = 0
+      then 0
+    else
+      cast(ceiling((1 + sqrt(8*n + 1))/2) as int)
   end as res
 from participants
