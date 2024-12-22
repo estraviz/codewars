362be57d438c3246b481f6fda518cb9c08652fe7
Javier Estraviz <javier.estraviz@gmail.com>
2024-12-23 00:26:34 +0100

    Added Rust solution to kata 'To square(root) or not to square(root) (8kyu)'

---
+++ 8_kyu/To square(root) or not to square(root)/rust/src/main.rs
@@ -0,0 +1,34 @@
+fn square_or_square_root(arr: &[u32]) -> Vec<u32> {
+    let mut res: Vec<u32> = Vec::new();
+
+    for num in arr.iter() {
+        let root: f64 = (*num as f64).sqrt().floor() as f64;
+        let res_elem: u32 = match f64::from(root * root) == (*num as f64) {
+            true => root as u32,
+            _ => num * num,
+        };
+        res.push(res_elem);
+    }
+
+    res
+}
+
+#[cfg(test)]
+mod tests {
+    use super::square_or_square_root;
+
+    fn dotest(arr: &[u32], expected: &[u32]) {
+        let actual = square_or_square_root(arr);
+        assert!(
+            actual == expected,
+            "Test failed with  arr = {arr:?}\nExpected {expected:?} but got {actual:?}"
+        );
+    }
+
+    #[test]
+    fn sample_tests() {
+        dotest(&[4, 3, 9, 7, 2, 1], &[2, 9, 3, 49, 4, 1]);
+        dotest(&[100, 101, 5, 5, 1, 1], &[10, 10201, 25, 25, 1, 1]);
+        dotest(&[1, 2, 3, 4, 5, 6], &[1, 4, 9, 2, 25, 36]);
+    }
+}
