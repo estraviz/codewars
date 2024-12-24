41d4e718b14ecb73fcf097e3414767810083e948
Javier Estraviz <javier.estraviz@gmail.com>
2024-12-25 00:23:04 +0100

    Added Rust solution to kata 'Add Length (8kyu)'

---
+++ 8_kyu/Add length/rust/src/main.rs
@@ -0,0 +1,40 @@
+fn add_length(s: &str) -> Vec<String> {
+    s.split(' ')
+        .map(|x| x.to_string() + " " + &x.len().to_string())
+        .collect()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::add_length;
+
+    fn dotest(s: &str, expected: &[String]) {
+        let actual = add_length(s);
+        assert!(
+            actual == expected,
+            "With s = \"{s}\"\nExpected {expected:?} but got {actual:?}"
+        )
+    }
+
+    #[test]
+    fn fixed_tests() {
+        dotest(
+            "apple ban",
+            &["apple 5", "ban 3"]
+                .iter()
+                .map(|x| x.to_string())
+                .collect::<Vec<_>>(),
+        );
+        dotest(
+            "you will win",
+            &["you 3", "will 4", "win 3"]
+                .iter()
+                .map(|x| x.to_string())
+                .collect::<Vec<_>>(),
+        );
+        dotest(
+            "y",
+            &["y 1"].iter().map(|x| x.to_string()).collect::<Vec<_>>(),
+        );
+    }
+}
