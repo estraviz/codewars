3b3506a0615167998aaf25d463f5835af7dc2a2e
Javier Estraviz <javier.estraviz@gmail.com>
2024-12-28 00:14:10 +0100

    Added refactor to Rust solution for kata 'Well of Ideas - Easy Version (8kyu)'

--- 8_kyu/Grasshopper - Basic Function Fixer/rust/target/debug/deps/rust-f18148e5eb002c72.d
+++
@@ -1,5 +0,0 @@
-/Users/javier/workspace/codewars/8_kyu/Grasshopper - Basic Function Fixer/rust/target/debug/deps/rust-f18148e5eb002c72: src/main.rs
-
-/Users/javier/workspace/codewars/8_kyu/Grasshopper - Basic Function Fixer/rust/target/debug/deps/rust-f18148e5eb002c72.d: src/main.rs
-
-src/main.rs:
