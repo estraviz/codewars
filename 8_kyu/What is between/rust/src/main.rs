19d48cf9ca8c2d203428edf1a574396f77677d92
Javier Estraviz <javier.estraviz@gmail.com>
2025-01-04 17:03:45 +0100

    Added Rust solution to kata 'What is between (8kyu)'

---
+++ 8_kyu/What is between/rust/src/main.rs
@@ -0,0 +1,28 @@
+fn between(a: i16, b: i16) -> Vec<i16> {
+    let mut output: Vec<i16> = vec![];
+
+    for i in a..=b {
+        output.push(i);
+    }
+
+    output
+}
+
+#[cfg(test)]
+mod tests {
+    use super::between;
+
+    fn dotest(a: i16, b: i16, expected: &[i16]) {
+        let actual = between(a, b);
+        assert!(
+            actual == expected,
+            "Test failed with a = {a}, b = {b}\nExpected {expected:?}\nBut got {actual:?}"
+        )
+    }
+
+    #[test]
+    fn test_basic() {
+        dotest(1, 4, &[1, 2, 3, 4]);
+        dotest(-2, 2, &[-2, -1, 0, 1, 2]);
+    }
+}
