2e21cd55978610e221af935764b54fd26d5c1311
Javier Estraviz <javier.estraviz@gmail.com>
2025-01-13 23:57:51 +0100

    Added Rust solution to kata 'Remove First and Last Character (8kyu)'

---
+++ 8_kyu/Remove First and Last Character/rust/src/main.rs
@@ -0,0 +1,18 @@
+pub fn remove_char(s: &str) -> String {
+    s.to_string()[1..s.to_string().len() - 1].to_string()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::remove_char;
+
+    #[test]
+    fn sample_cases() {
+        assert_eq!(remove_char("eloquent"), "loquen");
+        assert_eq!(remove_char("country"), "ountr");
+        assert_eq!(remove_char("person"), "erso");
+        assert_eq!(remove_char("place"), "lac");
+        assert_eq!(remove_char("ok"), "");
+        assert_eq!(remove_char("ooopsss"), "oopss");
+    }
+}
