16b236b09442c66f4bd45439e3124193a23ab05e
Javier Estraviz <javier.estraviz@gmail.com>
2025-01-18 08:35:36 +0100

    Added Rust solution to kata 'Multiplication table for number (8kyu)'

---
+++ 8_kyu/Multiplication table for number/rust/src/main.rs
@@ -0,0 +1,21 @@
+fn multi_table(n: u64) -> String {
+    let mut res = String::new();
+
+    for i in 1..=10 {
+        let s: String = format!("{} * {} = {}\n", i, n, i * n);
+        res.push_str(&s);
+    }
+
+    res.strip_suffix("\n").unwrap().to_string()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn basic() {
+        assert_eq!(multi_table(5), "1 * 5 = 5\n2 * 5 = 10\n3 * 5 = 15\n4 * 5 = 20\n5 * 5 = 25\n6 * 5 = 30\n7 * 5 = 35\n8 * 5 = 40\n9 * 5 = 45\n10 * 5 = 50");
+        assert_eq!(multi_table(1), "1 * 1 = 1\n2 * 1 = 2\n3 * 1 = 3\n4 * 1 = 4\n5 * 1 = 5\n6 * 1 = 6\n7 * 1 = 7\n8 * 1 = 8\n9 * 1 = 9\n10 * 1 = 10");
+    }
+}
