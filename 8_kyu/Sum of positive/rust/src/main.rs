1dafdca67bb274946261d41f573ff078f3f457f4
Javier Estraviz <javier.estraviz@gmail.com>
2025-01-30 23:40:11 +0100

    Added Rust solution to kata 'Sum of positive (8kyu)'

---
+++ 8_kyu/Sum of positive/rust/src/main.rs
@@ -0,0 +1,31 @@
+fn positive_sum(slice: &[i32]) -> i32 {
+    slice
+        .iter()
+        .map(|x| match *x {
+            x if x > 0 => x,
+            _ => 0,
+        })
+        .sum()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn some_examples() {
+        assert_eq!(positive_sum(&[1, 2, 3, 4, 5]), 15);
+        assert_eq!(positive_sum(&[1, -2, 3, 4, 5]), 13);
+        assert_eq!(positive_sum(&[-1, 2, 3, 4, -5]), 9);
+    }
+
+    #[test]
+    fn empty_list() {
+        assert_eq!(positive_sum(&[]), 0);
+    }
+
+    #[test]
+    fn all_negative() {
+        assert_eq!(positive_sum(&[-1, -2, -3, -4, -5]), 0);
+    }
+}
