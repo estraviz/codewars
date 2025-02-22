60ddee224861a6f3e42acb0980debf372711f15f
Javier Estraviz <javier.estraviz@gmail.com>
2025-02-23 00:15:17 +0100

    Added Rust solution to kata 'Quarter of the year (8kyu)'

---
+++ 8_kyu/Quarter of the year/rust/src/main.rs
@@ -0,0 +1,15 @@
+fn quarter_of(month: u8) -> u8 {
+    (month - 1) / 3 + 1
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn basic() {
+        assert_eq!(quarter_of(3), 1, "Month 3 = quarter 1");
+        assert_eq!(quarter_of(8), 3, "Month 8 = quarter 3");
+        assert_eq!(quarter_of(11), 4, "Month 11 = quarter 4");
+    }
+}
