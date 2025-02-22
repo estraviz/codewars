e656aa28d2e4f9b1e838656a47f2f3595e717ccc
Javier Estraviz <javier.estraviz@gmail.com>
2025-02-22 23:45:53 +0100

    Added Rust solution to kata 'Third Angle of a Triangle (8kyu)'

---
+++ 8_kyu/Third Angle of a Triangle/rust/src/main.rs
@@ -0,0 +1,16 @@
+fn other_angle(a: u32, b: u32) -> u32 {
+    180 - (a + b)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn example_tests() {
+        assert_eq!(other_angle(30, 60), 90);
+        assert_eq!(other_angle(60, 60), 60);
+        assert_eq!(other_angle(43, 78), 59);
+        assert_eq!(other_angle(10, 20), 150);
+    }
+}
